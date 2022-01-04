b9a195f3739d9a111eeac769e331fb0c8e1b1fca
thisaiveerapandian <thisaiveerapandian.1995@gmail.com>
2022-01-04 21:25:36 +0530

    minor changes

--- program/src/app/adults/shared/component/module-end-videos/module-end-videos.component.ts
+++ program/src/app/adults/shared/component/module-end-videos/module-end-videos.component.ts
@@ -54,30 +54,19 @@ export class ModuleEndVideosComponent implements OnInit {
     },
   ]
 
-  @Input() wisdomstoriesmoduleList: any = [
-    {
-      name: 'Ryan Had A Hippo’s Temper',
-      image: 'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/wisdom_stories/timothy-eberly-2JV3dbrFt-Y-unsplash.jpg',
-      link: ''
-    },
-    {
-      name: 'The Well-Being of an Alcoholic',
-      image: 'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/wisdom_stories/luke-southern-gW7QRXDSvec-unsplash.jpg',
-      link: ''
-    },
-    {
-      name: 'My Stepmother Humiliated Me',
-      image: 'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/wisdom_stories/kelly-sikkema-k0o-cw5Y_9s-unsplash.jpg',
-      link: ''
-    },
-    {
-      name: 'An Apple A Day Is An Eating Disorder',
-      image: 'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/wisdom_stories/gregory-pappas-rUc9hVE-L-E-unsplash.jpg',
-      link: ''
-    }
+  wisdomstoriesmoduleList: any = [
   ]
 
-  constructor(private router:Router,private service: AdultsService) { }
+  constructor(private router:Router,private service: AdultsService) {
+    let story = JSON.parse(JSON.stringify(localStorage.getItem('supportwisdomstories')));
+    story = JSON.parse(story)
+    this.wisdomstoriesmoduleList = story.slice(0, 4) 
+   }
+
+   viewstory(item){
+    localStorage.setItem("story",JSON.stringify(item))
+    this.router.navigate(['/wisdom-stories/view-stories'],{ queryParams: {sId: `${item['ScenarioID']}`}})
+  }
 
   ngOnInit() {
     if(this.saveUsername==false)
