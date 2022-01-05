81e13440a0a78b024d47bef99a614247ddc1f72a
Sana Sayyed <s29a9s93@gmail.com>
2022-01-05 18:11:37 +0530

    module end videos - get support now + solving global problems

--- program/src/app/adults/solving-global-problems/solving-global-problems.module.ts
+++ program/src/app/adults/solving-global-problems/solving-global-problems.module.ts
@@ -13,7 +13,11 @@ import { S72002Page } from './s72002/s72002.page';
 import { S72003Page } from './s72003/s72003.page';
 import { S72004Page } from './s72004/s72004.page';
 import { S72005Page } from './s72005/s72005.page';
-
+import { S72001p1Page } from './s72001p1/s72001p1.page';
+import { S72002p1Page } from './s72002p1/s72002p1.page';
+import { S72003p1Page } from './s72003p1/s72003p1.page';
+import { S72004p1Page } from './s72004p1/s72004p1.page';
+import { S72005p1Page } from './s72005p1/s72005p1.page';
 @NgModule({
   declarations: [
     S72001Page,
@@ -21,6 +25,11 @@ import { S72005Page } from './s72005/s72005.page';
     S72003Page,
     S72004Page,   
     S72005Page,
+    S72001p1Page,
+    S72002p1Page,
+    S72003p1Page,
+    S72004p1Page,
+    S72005p1Page,
   ],
   imports: [
     CommonModule,
