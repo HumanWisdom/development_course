a0d571a40837a306f3c2be08dde5fc8698819936
betsyjohn0112 <betsyjohn0112@gmail.com>
2022-01-19 16:32:17 +0530

    continue where you left bug resolved remaining

--- program/src/app/adults/program-guide/s35001/s35001.page.ts
+++ program/src/app/adults/program-guide/s35001/s35001.page.ts
@@ -136,10 +136,7 @@ export class S35001Page implements OnInit,OnDestroy {
 
   }
   ngOnDestroy(){
-    this.endTime = Date.now();
-    this.totalTime = this.endTime - this.startTime;
-    //console.log(this.totalTime,"total time")
-    //this.submitProgress()
+ 
 
 
 
