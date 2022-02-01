bbad0963f110ffdfc3680aff02d2aa88e9cc596d
PavishJain21 <jainpavish@gmail.com>
2022-02-01 22:11:51 +0530

    Resolve completed

--- program/src/app/coach/coach-appointments/coach-appointments.page.ts
+++ program/src/app/coach/coach-appointments/coach-appointments.page.ts
@@ -32,58 +32,37 @@ export class CoachAppointmentsPage implements OnInit {
     await this.apiService.getUpComingAppointment().subscribe(res => {
       getData = res;
       if (getData.length > 0) {
-        getData.map(res => {
-          res['BookingDates'] = res.Date.split(' ')[0].split('/')[2] + 
-                                '-' + 
-                                res.Date.split(' ')[0].split('/')[0] + 
-                                '-' + 
-                                res.Date.split(' ')[0].split('/')[1] + 
-                                ' ' + 
-                                moment(res.StartTime.split(' ')[1] + res.StartTime.split(' ')[2], 'hh:mm A').format('HH:mm A');
-          res['combineDate'] = moment(res.Date.split(' ')[0].split('/')[2] + 
-          '-' + 
-          res.Date.split(' ')[0].split('/')[0] + 
-          '-' + 
-          res.Date.split(' ')[0].split('/')[1] ).format('ddd, D MMM YYYY') + ' '+
-          moment(res.StartTime.split(' ')[1] + res.StartTime.split(' ')[2], 'hh:mm A').format('HH.mm A') + 
-          '-' + 
-          moment(res.EndTime.split(' ')[1] + res.EndTime.split(' ')[2], 'hh:mm A').format('HH.mm A')
-          ;
+        getData.map((res) => {
+          res['BookingDates'] = res.Date.split(' ')[0].split('/')[2] +
+            '-' +
+            res.Date.split(' ')[0].split('/')[0] +
+            '-' +
+            res.Date.split(' ')[0].split('/')[1] +
+            ' ' +
+            moment(res.StartTime.split(' ')[1] + res.StartTime.split(' ')[2], 'hh:mm A').format('HH:mm A');
+          res['combineDate'] = moment(res.Date.split(' ')[0].split('/')[2] +
+            '-' +
+            res.Date.split(' ')[0].split('/')[0] +
+            '-' +
+            res.Date.split(' ')[0].split('/')[1]).format('ddd, D MMM YYYY') + ' ' +
+            moment(res.StartTime.split(' ')[1] + res.StartTime.split(' ')[2], 'hh:mm A').format('HH.mm A') +
+            '-' +
+            moment(res.EndTime.split(' ')[1] + res.EndTime.split(' ')[2], 'hh:mm A').format('HH.mm A');
         });
       }
-
-      const date = new Date();
-      const n = date.toDateString();
-      const time = date.toLocaleTimeString();
-
-      const dateToCheckFor = moment(n).format('YYYY-MM-DD') + ' ' + moment(time, 'hh:mm A').format('HH:mm');
-
       getData.forEach(date => {
-        let diff = moment(date['BookingDates']).diff(moment(dateToCheckFor), 'minutes');
-        if (diff > 0) {
-          this.upcomingDate.push(date);
-          if (this.nearestDate) {
-            if (moment(date['BookingDates']).diff(moment(this.nearestDate?.BookingDates), 'minutes') < 0) {
-              this.nearestDate = date;
-            }
-          } else {
-            this.nearestDate = date;
-          }
-        } else{
+        if(date.CancelledBy == "0" && date.CancelledDateTime == "") {
+        if (date.Completed == '1') {
           this.previousDate.push(date);
+        } else {
+          this.upcomingDate.push(date);
         }
+      }
       });
       this.isAPICalling = false;
-      console.log('Upcoming Uppointment', this.upcomingDate);
-      console.log('Previous Uppointment', this.previousDate);
-      // if (this.nearestDate !== null) {
-      //   this.appointmentDates = moment(this.nearestDate?.BookingDates).format('dddd, D MMMM YYYY, hh A')
-      // }
 
-      // console.log('Appointment dates are:', this.appointmentDates);
     }, error => {
       this.isAPICalling = false;
     });
-    // + ' - ' +  moment(this.nearestDate?.EndTime.split(' ')[1]+this.nearestDate?.EndTime.split(' ')[2], 'hh:mm A').format('HH:mm A');
   }
 }
