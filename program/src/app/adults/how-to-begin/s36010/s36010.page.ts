fa952c4669ed615d0a86c39e2f430f1d845f3fea
ashwinirathod123 <86161974+ashwinirathod123@users.noreply.github.com>
2022-02-04 13:11:07 +0530

    how-to-begin

--- program/src/app/adults/how-to-begin/s36010/s36010.page.ts
+++ program/src/app/adults/how-to-begin/s36010/s36010.page.ts
@@ -85,6 +85,7 @@ createScreen(){
   submitProgress(){
     this.endTime = Date.now();
     this.totalTime = this.endTime - this.startTime;
+    this.router.navigate(['/how-to-begin/s36011'])
     this.service.submitProgressText({
       "ScrNumber":this.screenNumber,
       "UserId":this.userId,
@@ -102,7 +103,7 @@ createScreen(){
       ()=>{
         //this.router.navigate(['/adults/conditioning/s2336010'])
       })
-      this.router.navigate(['/how-to-begin/s36011'])
+     
     
 
   }
