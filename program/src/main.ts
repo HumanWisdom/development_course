ced79c4c69e36da38b9bed344f590100a53391e4
PavishJain21 <jainpavish@gmail.com>
2022-02-07 21:13:26 +0530

    first chat commit

--- program/src/main.ts
+++ program/src/main.ts
@@ -1,12 +1,22 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { CometChat } from '@cometchat-pro/chat';
 
 import { AppModule } from './app/app.module';
+import { COMETCHAT_CONSTANTS } from './app/coach/CONSTS';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+const appSetting = new CometChat.AppSettingsBuilder().setRegion(COMETCHAT_CONSTANTS.REGION).subscribePresenceForAllUsers().build();
+CometChat.init(COMETCHAT_CONSTANTS.APP_ID, appSetting).then(() => {
+
+  console.log('app is ready to work');
+
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+}, (error) => {
+  console.log('Error In Init', error);
+});
