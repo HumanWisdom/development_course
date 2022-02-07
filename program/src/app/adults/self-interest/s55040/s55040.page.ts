2a89264841ee2de7836effe56720432e5e878a69
ashwinirathod123 <86161974+ashwinirathod123@users.noreply.github.com>
2022-02-07 15:42:18 +0530

    self-interest

--- program/src/app/adults/self-interest/s55040/s55040.page.ts
+++ program/src/app/adults/self-interest/s55040/s55040.page.ts
@@ -91,7 +91,8 @@ createScreen(){
    
     this.endTime = Date.now();
     this.totalTime = this.endTime - this.startTime;
-
+    this.router.navigate(['/self-interest/s55041'])
+   
     this.service.submitProgressAv({
       "ScrNumber":this.screenNumber,
       "UserId":this.userId,
@@ -106,8 +107,7 @@ createScreen(){
         this.bookmarkList=res.GetBkMrkScr.map(a=>parseInt(a.ScrNo))
         localStorage.setItem("bookmarkList",JSON.stringify(this.bookmarkList))
       })
-    
-    this.router.navigate(['/self-interest/s55041'])
+     
    
 
   }
