a50dd7c4cb01b59f8085f04665d0c2911d1eb2bd
ashwinirathod123 <86161974+ashwinirathod123@users.noreply.github.com>
2022-02-08 15:02:34 +0530

    Update s28003.page.ts

--- program/src/app/adults/nature/s28003/s28003.page.ts
+++ program/src/app/adults/nature/s28003/s28003.page.ts
@@ -92,7 +92,7 @@ createScreen(){
    
     this.endTime = Date.now();
     this.totalTime = this.endTime - this.startTime;
-
+    this.router.navigate(['/nature/s28004'])
     this.service.submitProgressAv({
       "ScrNumber":this.screenNumber,
       "UserId":this.userId,
