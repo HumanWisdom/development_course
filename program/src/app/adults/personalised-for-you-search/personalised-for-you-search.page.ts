90b4ec80e8f10570b62c81b5f94e4956c9570c48
thisaiveerapandian <thisaiveerapandian.1995@gmail.com>
2022-04-17 16:58:30 +0530

    personalised for you search changes

--- program/src/app/adults/personalised-for-you-search/personalised-for-you-search.page.ts
+++ program/src/app/adults/personalised-for-you-search/personalised-for-you-search.page.ts
@@ -1,5 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { AdultsService } from '../adults.service';
 
 @Component({
   selector: 'app-personalised-for-you-search',
@@ -8,11 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalisedForYouSearchPage implements OnInit {
   personalisedforyou = []
+  indList = []
 
-  constructor(private route: Router) {
+  constructor(private route: Router, private aservice:AdultsService,) {
     if(localStorage.getItem('perapidata')) {
       let pers = JSON.parse(localStorage.getItem('perapidata'));
       this.personalisedforyou = pers;
+      this.personalisedforyou.forEach((d) => {
+        if(d['active']){
+          this.indList.push(d['id'])
+        }
+      })
     }
    }
 
@@ -20,8 +27,40 @@ export class PersonalisedForYouSearchPage implements OnInit {
     
   }
 
-  clickbtn(name) {
-    this.route.navigate(['/adults/overcome-stress'])
+  clickbtn(name, val = '', event, ind, id) {
+    if(val === '') {
+      this.route.navigate(['/adults/overcome-stress'])
+    }else {
+     let fill =  this.personalisedforyou.filter((d) => d['name'] === name);
+     const index = this.indList.indexOf(id);
+     if(fill[0]['active']) {
+      if (index > -1) {
+        this.indList.splice(index, 1);
+      }
+     }else {
+       this.indList.push(id)
+     }
+     let reqpay = this.indList.join();
+    this.aservice.postUserpreference(reqpay).subscribe((res) => {
+        if(res) {
+          fill[0]['active'] = fill[0]['active'] ? false : true;
+          this.personalisedforyou.splice(ind, 1)
+          if(fill[0]['active']) {
+            this.personalisedforyou.unshift(fill[0])
+          }else {
+            this.personalisedforyou.push(fill[0])
+          }
+          this.indList = []
+          this.personalisedforyou.forEach((d) => {
+            if(d['active']){
+              this.indList.push(d['id'])
+            }
+          })
+          localStorage.setItem('perapidata', JSON.stringify(this.personalisedforyou));
+        }
+    })
+      // event.preventDefault();
+    }
   }
 
 }
