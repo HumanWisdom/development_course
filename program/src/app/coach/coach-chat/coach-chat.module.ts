51867e3d24d62d324d8b73d4329dfd207ef1504b
PavishJain21 <jainpavish@gmail.com>
2022-05-11 21:54:25 +0530

    removed comet-chat

--- program/src/app/coach/coach-chat/coach-chat.module.ts
+++ program/src/app/coach/coach-chat/coach-chat.module.ts
@@ -1,18 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 import { Routes, RouterModule } from '@angular/router';
 import { CoachChatComponent } from './coach-chat.component';
-import { CometChatUI } from '../cometchat-pro-angular-ui-kit/CometChatWorkspace/src/components/CometChatUI/CometChat-Ui/cometchat-ui.module';
-import { CometChatConversationListWithMessages } from '../cometchat-pro-angular-ui-kit/CometChatWorkspace/src/components/Chats/CometChat-conversation-list-with-messages/cometchat-conversation-list-with-messages.module';
-import { CometChatGroupListWithMessages } from '../cometchat-pro-angular-ui-kit/CometChatWorkspace/src/components/Groups/CometChat-group-list-with-messages/cometchat-group-list-with-messages.module';
-import { CometChatUserListWithMessages } from '../cometchat-pro-angular-ui-kit/CometChatWorkspace/src/components/Users/CometChat-user-list-with-messages/cometchat-user-list-with-messages.module';
-import { CometChatConversationList } from '../cometchat-pro-angular-ui-kit/CometChatWorkspace/src/components/Chats/CometChat-conversation-list/cometchat-conversation-list.module';
-import { CometChatGroupList } from '../cometchat-pro-angular-ui-kit/CometChatWorkspace/src/components/Groups/CometChat-group-list/cometchat-group-list.module';
-import { CometChatUserList } from '../cometchat-pro-angular-ui-kit/CometChatWorkspace/src/components/Users/CometChat-user-list/cometchat-user-list.module';
-import { CometChatAvatar } from '../cometchat-pro-angular-ui-kit/CometChatWorkspace/src/components/Shared/CometChat-avatar/cometchat-avatar.module';
-import { CometChatMessages} from '../cometchat-pro-angular-ui-kit/CometChatWorkspace/src/components/Messages/CometChat-messages/cometchat-messages.module'
 import { CommonModule } from '@angular/common';
 import { BottomNavigationComponent } from 'src/app/adults/shared/component/bottom-navigation/bottom-navigation.component';
 import { SharedModule } from 'src/app/adults/shared/shared.module';
@@ -30,15 +20,6 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    CometChatUI,
-    CometChatConversationListWithMessages,
-    CometChatGroupListWithMessages,
-    CometChatUserListWithMessages,
-    CometChatConversationList,
-    CometChatGroupList,
-    CometChatUserList,
-    CometChatAvatar,
-    CometChatMessages,
     SharedModule,
     RouterModule.forChild(routes)
   ],
