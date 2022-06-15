dce2689b457067f336f58db964f7ded994c7dfd0
Sana Sayyed <s29a9s93@gmail.com>
2022-06-15 17:14:00 +0530

    In the multiple select screen, when a box is checked, the entire bar changes to opaque yellow (please refer to the design)-62 files modified

--- program/src/app/adults/criticism/s365/s365.page.ts
+++ program/src/app/adults/criticism/s365/s365.page.ts
@@ -52,14 +52,16 @@ export class S365Page implements OnInit {
     this.sessionOption365=[]
     this.createScreen()
     this.startTime = Date.now();
-    for(var i=0;i<this.qrList.ListOfQueOpts.length;i++)
-    {
-      this.qrList.ListOfQueOpts[i].OptId=parseInt(this.qrList.ListOfQueOpts[i].OptId)
+    if(this.qrList.ListOfQueOpts) {
+      for(var i=0;i<this.qrList.ListOfQueOpts.length;i++)
+      {
+        this.qrList.ListOfQueOpts[i].OptId=parseInt(this.qrList.ListOfQueOpts[i].OptId)
 
+      }
     }
-      
-   
-    this.questionA=this.qrList.ListOfQueOpts
+
+
+    this.questionA=this.qrList?.ListOfQueOpts
     
     this.question=this.findQuestion(80).Question
     this.optionList=this.findQuestion(80).optionList
@@ -73,6 +75,17 @@ export class S365Page implements OnInit {
     {this.userId=JSON.parse(sessionStorage.getItem("userId"))}
     else
       {this.userId=JSON.parse(localStorage.getItem("userId"))}
+  }
+
+  ngAfterViewInit(): void {
+    if(this.optionList && this.sessionOption365) {
+      this.optionList.forEach((d) => {
+        if(this.sessionOption365.includes(d['OptId'])) {
+          document.getElementById(d['OptStr']).style.backgroundColor = '#FFC455';
+        }
+      }) 
+    }   
+
   }
 
   createScreen(){
@@ -118,14 +131,16 @@ export class S365Page implements OnInit {
     return({"Question":question,"optionList":this.optionList})
   }
 
- selectOption(id,e){
+ selectOption(id,e, divid){
    console.log(id,e)
    if(e==true)
    {
+    document.getElementById(divid).style.backgroundColor = '#FFC455';
      this.sendOption.push(id)
    }
    else if(e==false)
    {
+    document.getElementById(divid).style.backgroundColor = 'rgba(255,255,255,0.75)';
     this.sendOption.forEach((element,index)=>{
       if(element==id) this.sendOption.splice(index,1);
    });
@@ -133,7 +148,6 @@ export class S365Page implements OnInit {
    console.log(this.sendOption)
    sessionStorage.setItem("sessionOption365",JSON.stringify(this.sendOption))
   
-
  }
 
  submitProgress(){
@@ -159,15 +173,17 @@ prev(){
 
 
 }
-  sessionFetch(id){
+  sessionFetch(id, divid){
     if(this.sessionOption365.includes(id))
     {
-
+      // document.getElementById(divid).style.backgroundColor = '#FFC455';
       return true
     }
-      
-    else
+
+    else {
+      // document.getElementById(divid).style.backgroundColor = 'rgba(255,255,255,0.75)';
       return false
+    }
   }
   
   ngOnDestroy(){
