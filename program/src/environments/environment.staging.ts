e0ba6a1e8b25d483f3830628485e50a1705d542e
betsyjohn0112 <betsyjohn0112@gmail.com>
2022-06-28 12:35:05 +0530

    Created environment Variables for StripeKey for switching between Staging and production
    Created environment Variables for StripeKey for switching between Staging and production

--- program/src/environments/environment.staging.ts
+++ program/src/environments/environment.staging.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiURL: "https://staging.humanwisdom.info/api"
+    apiURL: "https://staging.humanwisdom.info/api",
+    stripeKey: "pk_test_51IDyEyLodCYBgHN86w4iS8izVNRW5BrBHRvNR5hamoNsCx1ccQWEMKVSSONQKVqHyFh5FWuUXTEFqyPdMjc2Nld200mJgPGVrl"
 };
   
