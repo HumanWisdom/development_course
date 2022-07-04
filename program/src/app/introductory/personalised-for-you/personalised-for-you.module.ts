5ba48bd6e6ccb475a318d647612359598562b34e
thisaiveerapandian <thisaiveerapandian.1995@gmail.com>
2022-07-04 17:31:01 +0530

    optimised adult dashboard and intro screen

--- program/src/app/introductory/personalised-for-you/personalised-for-you.module.ts
+++ program/src/app/introductory/personalised-for-you/personalised-for-you.module.ts
@@ -1,20 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
 import { PersonalisedForYouPageRoutingModule } from './personalised-for-you-routing.module';
-
 import { PersonalisedForYouPage } from './personalised-for-you.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule,
     PersonalisedForYouPageRoutingModule
   ],
   declarations: [PersonalisedForYouPage]
 })
-export class PersonalisedForYouPageModule {}
+export class PersonalisedForYouPageModule { }
