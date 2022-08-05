1e4a8c0fdf3c0dda85108f3f43968322ebcea6bd
Sana Sayyed <s29a9s93@gmail.com>
2022-08-05 20:13:03 +0530

    partnership partial routes added

--- program/src/app/adults/partnership-webpage/partnership-index/partnership-index.page.ts
+++ program/src/app/adults/partnership-webpage/partnership-index/partnership-index.page.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit } from '@angular/core';
 @Component({
   selector: 'HumanWisdom-partnership-index',
   templateUrl: './partnership-index.page.html',
@@ -7,9 +6,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartnershipIndexPage implements OnInit {
 
+  public isHowItWorks:boolean=false;
+  public isOtherBenefits:boolean=false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  scroll_to_obs(): void 
+  {
+    this.isHowItWorks=true;
+    this.isOtherBenefits=true;
+
+    setTimeout(() => {
+      window.scrollTo({
+        behavior: 'smooth',
+        top:
+        document.getElementById('pa_other_benefits').getBoundingClientRect().top -
+          document.body.getBoundingClientRect().top -
+          150,
+      })
+    }, 100);
+  }
+
+  enabledtab(string)
+  {
+    if(string=="whybepartner")
+    {
+      this.isHowItWorks=false;
+    }
+    else
+    {
+      this.isHowItWorks=true;
+    }
+  }
+
+  otherBenefitsClick()
+  {
+    if(this.isOtherBenefits)
+    {
+      this.isOtherBenefits=false;
+    }
+    else
+    {
+      this.isOtherBenefits=true;
+    }
   }
 
 }
