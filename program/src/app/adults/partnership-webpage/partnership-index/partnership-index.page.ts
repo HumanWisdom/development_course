e96a7b36bb954314d6b5873595774b6db928f584
Sana Sayyed <s29a9s93@gmail.com>
2022-08-05 12:09:46 +0530

    partnership-webpage - index + illustration screens

---
+++ program/src/app/adults/partnership-webpage/partnership-index/partnership-index.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'HumanWisdom-partnership-index',
+  templateUrl: './partnership-index.page.html',
+  styleUrls: ['./partnership-index.page.scss'],
+})
+export class PartnershipIndexPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
