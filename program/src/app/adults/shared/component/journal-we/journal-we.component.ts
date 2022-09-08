4a08873dcfe8f8c3dbd06c07155d1e6817a8e53b
thisaiveerapandian <thisaiveerapandian.1995@gmail.com>
2022-09-08 15:16:06 +0530

    journal reuse component

---
+++ program/src/app/adults/shared/component/journal-we/journal-we.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { AdultsService } from 'src/app/adults/adults.service';
+
+@Component({
+  selector: 'HumanWisdom-journal-we',
+  templateUrl: './journal-we.component.html',
+  styleUrls: ['./journal-we.component.scss'],
+})
+export class JournalWeComponent implements OnInit {
+
+  enabletick = false;
+  userId: any
+  note = ''
+  btnText = 'Add to journal'
+  t = new Date()
+  minDate = this.t.getFullYear() + "-" + this.addZero(this.t.getMonth() + 1) + "-" + this.addZero(this.t.getDate())
+
+  constructor(private service: AdultsService) { }
+
+  ngOnInit() {
+    this.userId = JSON.parse(sessionStorage.getItem("userId"))
+  }
+
+  addjournal() {
+    if (!this.enabletick) {
+      this.service.addJournal({
+        "JournalId": 0,
+        "JDate": "2022-09-08",
+        "Title": "Todays Tasks",
+        "Notes": this.note,
+        "UserId": this.userId
+      }).subscribe(res => {
+        this.enabletick = true;
+        this.btnText = 'Added to journal';
+      }, error => {
+        console.log(error)
+      })
+    }
+  }
+
+  addZero(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+}
