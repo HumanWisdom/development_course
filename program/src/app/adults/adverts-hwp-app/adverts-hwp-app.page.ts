b24c78679ad9443aeb203092c74c02d580f5ff27
Sana Sayyed <s29a9s93@gmail.com>
2022-10-18 12:04:23 +0530

    ui changes 1. Check the intro -subscription options page - on hovering entire box turning white & text wasn’t visible 2. Text replacement a. Upgrade to premium -> subscribe to premium b. Subscribe for newsletter -> signup for newsletter 3. Activation Code popup  taking complete screen on desktop mode, and no cancel button also visible .. from add to cart, view cart 4. Advert student+work - package circles added to design course 5. Adverts screens added to development course repo a. Hwp b. Hwp-app c. About d. Work e. Student

---
+++ program/src/app/adults/adverts-hwp-app/adverts-hwp-app.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'HumanWisdom-adverts-hwp-app',
+  templateUrl: './adverts-hwp-app.page.html',
+  styleUrls: ['./adverts-hwp-app.page.scss'],
+})
+export class AdvertsHwpAppPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ program/src/app/adults/adverts-work/adverts-work.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'HumanWisdom-adverts-work',
+  templateUrl: './adverts-work.page.html',
+  styleUrls: ['./adverts-work.page.scss'],
+})
+export class AdvertsWorkPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
