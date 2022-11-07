7bd9e1e2fe75c7e304a6a0e6ee0db755ce6712eb
betsyjohn0112 <97895908+betsyjohn0112@users.noreply.github.com>
2022-11-07 16:24:07 +0530

    Revert "Update adult-dashboard.page.html"

--- program/src/app/log-event.service.ts
+++ program/src/app/log-event.service.ts
@@ -12,11 +12,15 @@ export class LogEventService {
         private deviceService: DeviceDetectorService
     ) { }
 
-    logEvent(eventname: string) {
+    logEvent(eventname: string, param: string, val: string) {
+        let obj = {};
         let name = localStorage.getItem('name') ? localStorage.getItem('name') : 'Guest User'
         let device_info: any = this.deviceService.getDeviceInfo()
-        this.analytics.logEvent(eventname, { UserName: name });
-        this.analytics.logEvent(eventname, { DeviceOS: device_info.os });
-        this.analytics.logEvent(eventname, { DeviceBrowser: device_info.browser });
+        this.analytics.logEvent(eventname, { username: name });
+        this.analytics.logEvent(eventname, { deviceOS: device_info.os });
+        this.analytics.logEvent(eventname, { deviceBrowser: device_info.browser });
+        this.analytics.logEvent(eventname, { device: device_info.device });
+        obj[param] = val;
+        this.analytics.logEvent(eventname, obj);
     }
 }
