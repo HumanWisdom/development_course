e12bc62476368d4b2c95ede441be4169ce4e866b
Sana Sayyed <s29a9s93@gmail.com>
2022-11-09 17:05:42 +0530

    ui changes
    1. Wisdom exercise images changed with Nidhi mam’s images
    2. Landing Page common footer component
    3. Adverts - the login menu is routing to live site from staging

---
+++ program/src/app/adults/shared/component/tn-advert/tn-advert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tn-advert',
+  templateUrl: './tn-advert.component.html',
+  styleUrls: ['./tn-advert.component.scss'],
+})
+export class TnAdvertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
