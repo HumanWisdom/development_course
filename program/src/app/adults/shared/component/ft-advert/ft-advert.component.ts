5088495f6e2133be3749a9ad67306bcb7e6f8e63
pavishjain21 <jainpavish@gmail.com>
2022-11-15 21:40:07 +0530

    added logged in and subscriber condition

--- program/src/app/adults/shared/component/ft-advert/ft-advert.component.ts
+++ program/src/app/adults/shared/component/ft-advert/ft-advert.component.ts
@@ -8,8 +8,18 @@ import { OnboardingService } from 'src/app/onboarding/onboarding.service';
   styleUrls: ['./ft-advert.component.scss'],
 })
 export class FtAdvertComponent implements OnInit {
-
-  constructor(public router: Router,public service:OnboardingService) { }
+   isLoggedIn:boolean=false;
+   isSubscribe:boolean=false;
+   enablepopup:boolean=false;
+  constructor(public router: Router,public service:OnboardingService) { 
+    let res = localStorage.getItem("isloggedin")
+    if(res=='T'){
+     this.isLoggedIn=true;
+    }
+    let popup = JSON.parse(localStorage.getItem("Subscriber"))
+    if(popup === 1) this.enablepopup = true
+    this.isSubscribe = popup === 0 ? false : true;
+  }
 
   ngOnInit( ) {}
   RedeemCode(){
