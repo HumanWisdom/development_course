0b592d979e8b2f561b4918647da9c9bbf36a2cb2
pavishjain21 <jainpavish@gmail.com>
2022-11-25 00:49:43 +0530

    added share functionality

--- program/src/app/adults/wisdom-exercise/s75001/s75001.page.ts
+++ program/src/app/adults/wisdom-exercise/s75001/s75001.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgNavigatorShareService } from 'ng-navigator-share';
 
 @Component({
   selector: 'HumanWisdom-s75001',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./s75001.page.scss'],
 })
 export class S75001Page implements OnInit {
-
-  constructor() { }
-
+  path = this.router.url
+  constructor(public ngNavigatorShareService: NgNavigatorShareService,
+    private router: Router,) { }
+ 
   ngOnInit() {
+  }
+  share(){
+    this.ngNavigatorShareService.share({
+      title: 'HumanWisdom Program',
+      text: 'Hey, check out the HumanWisdom Program',
+      url: "https://humanwisdom.me/course"+this.path
+    }).then( (response) => {
+      console.log(response);
+    })
+    .catch( (error) => {
+      console.log(error);
+    });
   }
 
 }
