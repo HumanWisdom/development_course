208c33ca147a04e27c75fc972bc79754937284f2
thisaiveerapandian <thisaiveerapandian.1995@gmail.com>
2022-12-02 00:17:33 +0300

    audio meditation page changes

--- program/src/app/adults/audio-meditation/audio-meditation-routing.module.ts
+++ program/src/app/adults/audio-meditation/audio-meditation-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ActiveGuard } from 'src/app/active.guard';
 
 import { S51000Page } from './s51000/s51000.page';
+import { S51002Page } from './s51002/s51002.page';
 
 const routes: Routes = [
   {
@@ -13,6 +14,11 @@ const routes: Routes = [
     path: 's51000',
     canActivate: [ActiveGuard],
     component: S51000Page,
+  },
+  {
+    path: 'audio',
+    canActivate: [ActiveGuard],
+    component: S51002Page,
   }
 
 ];
---
+++ program/src/app/adults/audio-meditation/s51002/s51002.page.ts
@@ -0,0 +1,117 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdultsService } from "../../adults.service";
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-s51002',
+  templateUrl: './s51002.page.html',
+  styleUrls: ['./s51002.page.scss'],
+})
+export class S51002Page implements OnInit {
+
+  bg_tn = "bg_dark_blue"
+  bg_cft = "bg_dark_blue"
+  bg = "dark_blue_w1"
+  title = localStorage.getItem("meditationtitle")
+  mediaAudio = localStorage.getItem("meditationaudio")
+  audioLink = this.mediaAudio
+
+  transcriptPage = ""
+  toc = "guided-meditation/s51000"
+  bookmark = 0
+  path = this.router.url
+  avDuration: any
+  userId: any
+  saveUsername = JSON.parse(localStorage.getItem("saveUsername"))
+  screenType = localStorage.getItem("audio")
+  moduleId = localStorage.getItem("moduleId")
+  screenNumber = 51002
+  startTime: any
+  endTime: any
+  totalTime: any
+
+  bookmarkList = JSON.parse(localStorage.getItem("bookmarkList"))
+
+  constructor(private router: Router,
+    private service: AdultsService,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    if (this.saveUsername == false) { this.userId = JSON.parse(sessionStorage.getItem("userId")) }
+    else { this.userId = JSON.parse(localStorage.getItem("userId")) }
+    this.startTime = Date.now();
+
+    this.startTime = Date.now();
+    this.createScreen()
+    if (JSON.parse(sessionStorage.getItem("bookmark51002")) == 0)
+      this.bookmark = 0
+    else if (this.bookmarkList.includes(this.screenNumber) || JSON.parse(sessionStorage.getItem("bookmark51002")) == 1)
+      this.bookmark = 1
+
+  }
+
+  createScreen() {
+    this.service.createScreen({
+      "ScrId": 0,
+      "ModuleId": this.moduleId,
+      "GSetID": this.screenType,
+      "ScreenNo": this.screenNumber
+    }).subscribe(res => {
+
+    })
+
+
+  }
+
+  receiveBookmark(e) {
+    console.log(e)
+    if (e == true)
+      this.bookmark = 1
+    else
+      this.bookmark = 0
+    sessionStorage.setItem("bookmark51002", JSON.stringify(this.bookmark))
+  }
+
+  receiveAvDuration(e) {
+    console.log(e)
+    this.avDuration = e
+
+  }
+
+  submitProgress() {
+
+    this.endTime = Date.now();
+    this.totalTime = this.endTime - this.startTime;
+    this.router.navigate(['/adults/guided-meditation/s51003'])
+    this.service.submitProgressAv({
+      "ScrNumber": this.screenNumber,
+      "UserId": this.userId,
+      "BookMark": this.bookmark,
+      "ModuleId": this.moduleId,
+      "screenType": this.screenType,
+      "timeSpent": this.totalTime,
+      "avDuration": this.avDuration
+    }).subscribe(res => {
+
+      this.bookmarkList = res.GetBkMrkScr.map(a => parseInt(a.ScrNo))
+      localStorage.setItem("bookmarkList", JSON.stringify(this.bookmarkList))
+    })
+
+
+
+
+  }
+  prev() {
+    this.location.back()
+
+
+  }
+  ngOnDestroy() {
+    localStorage.setItem("totalTime51002", this.totalTime)
+    localStorage.setItem("avDuration51002", this.avDuration)
+
+  }
+}
+
