05cf62546994242b360d99f9937b31e642d734e6
Pavish <96987710+PavishJain21@users.noreply.github.com>
2023-01-03 20:26:02 +0530

    removed program and admin project

--- program/src/app/adults/partnership-report/mask.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MaskPipe } from '../shared/pipe/mask.pipe';
-
-describe('MaskPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MaskPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
