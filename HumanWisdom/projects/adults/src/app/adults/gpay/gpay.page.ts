b4ad97a4903d2fda9d62e2efed8ceb59344548f7
PavishJain21 <jainpavish@gmail.com>
2023-02-03 13:12:36 +0530

    GPAY CHANGES

--- HumanWisdom/projects/adults/src/app/adults/gpay/gpay.page.ts
+++ HumanWisdom/projects/adults/src/app/adults/gpay/gpay.page.ts
@@ -1,34 +1,85 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { UPIHandlerService } from '../upihandler.service';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-gpay',
-  templateUrl: './gpay.page.html',
-  styleUrls: ['./gpay.page.scss']
+  template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          Google Pay Example
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+      <ion-button (click)="payWithGooglePay()">Pay with Google Pay</ion-button>
+  `
 })
-export class GpayPage implements OnInit {
- paymentStatus:string;
+export class GpayPage {
+  payWithGooglePay() {
+    const details = {
+      total: {
+        label: 'Total',
+        amount: {
+          currency: 'INR',
+          value: '1.00', // sample amount
+        },
+      },
+      displayItems: [{
+        label: 'Original Amount',
+        amount: {
+          currency: 'INR',
+          value: '1.00',
+        },
+      }],
+    };
 
-  ngOnInit(): void {
-    
+
+    if (this.isIOS()) {
+      // Handle Apple Pay on iOS
+    } else if (this.isAndroid() && window.PaymentRequest) {
+      // Handle Google Pay on Android
+      const paymentRequest = new PaymentRequest([{
+        supportedMethods: 'https://google.com/pay',
+        data: {
+          environment: 'TEST',
+          apiVersion: 2,
+          apiVersionMinor: 0,
+          merchantInfo: {
+            merchantId: 'BCR2DN4T7CCPJ2AE',
+            merchantName: 'KUNDAN STEEL AND HARDWARE'
+          },
+          transactionInfo: {
+            totalPriceStatus: 'FINAL',
+            totalPrice: '1.00',
+            currencyCode: 'INR'
+          },
+          allowedCardNetworks: ['VISA', 'MASTERCARD', 'AMEX'],
+          paymentMethodTokenizationParameters: {
+            tokenizationType: 'PAYMENT_GATEWAY',
+            parameters: {
+              gateway: 'example',
+              gatewayMerchantId: 'exampleGatewayMerchantId'
+            }
+          }
+        }
+      }],details);
+  
+      paymentRequest.show().then(paymentResponse => {
+        console.log(paymentResponse);
+        paymentResponse.complete('success');
+      }).catch(error => {
+        console.error(error);
+      });
+    } else {
+      console.error('Google Pay is not supported on this platform');
+    }
   }
 
-  constructor(private router: Router,private upiHandler:UPIHandlerService) {
-    // this.router.events.subscribe((event) => {
-    //   if (event instanceof NavigationEnd) {
-    //     this.handleUPIResponse(event.url);
-    //   }
-    // });
-    this.initiateUPIPayment();
+  isAndroid() {
+    return navigator.userAgent.match(/Android/i);
   }
 
-
-  handleUPIResponse(url: string) {
-
+  isIOS() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
   }
-
-  initiateUPIPayment() {
-    this.upiHandler.openUPIApp('9828173308@okbizaxis');
-  }
-
 }
