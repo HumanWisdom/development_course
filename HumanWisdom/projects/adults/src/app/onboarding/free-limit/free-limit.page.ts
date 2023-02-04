d5303492d37ece6a818e20b2473ebed876c16b7a
thisaiveerapandian <thisaiveerapandian.1995@gmail.com>
2023-02-04 15:00:50 +0300

    live changes

--- HumanWisdom/projects/adults/src/app/onboarding/free-limit/free-limit.page.ts
+++ HumanWisdom/projects/adults/src/app/onboarding/free-limit/free-limit.page.ts
@@ -1,42 +1,66 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import {Location } from '@angular/common'
-import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-free-limit',
   templateUrl: './free-limit.page.html',
   styleUrls: ['./free-limit.page.scss'],
 })
-export class FreeLimitPage implements OnInit, AfterViewInit {
-  @ViewChild('enablebtn') enableotpmodal:ElementRef;
-  @ViewChild('closemodal') closemodal:ElementRef;
+export class FreeLimitPage implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('enablebtn') enableotpmodal: ElementRef;
+  @ViewChild('closemodal') closemodal: ElementRef;
+  navigationSubs = new Subscription();
 
   isloggedIn = false;
 
-  constructor( private location:Location, private router: Router ) { }
+  constructor(private location: Location, private router: Router) { }
 
   ngOnInit() {
     let userid = localStorage.getItem('isloggedin');
-    if(userid === 'T') {
+    if (userid === 'T') {
       this.isloggedIn = true
     }
   }
 
   ngAfterViewInit(): void {
-    this.enableotpmodal.nativeElement.click()    
+    this.enableotpmodal.nativeElement.click()
   }
 
-  backClick(){
-   this.location.back()
+  backClick() {
+    let previousUrl: string = null;
+    let currentUrl: string = null;
+    this.navigationSubs = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      previousUrl = currentUrl;
+      currentUrl = event.url;
+      this.backroute(previousUrl);
+    });
+    this.location.back()
+  }
+
+  backroute(previousUrl) {
+    if (previousUrl) {
+      this.location.back()
+    } else {
+      this.router.navigate(['/adults/adult-dashboard'])
+    }
+    this.closemodal.nativeElement.click();
   }
 
   loginpage(issub = false) {
     this.closemodal.nativeElement.click()
-    if(issub && this.isloggedIn) {
+    if (issub && this.isloggedIn) {
       this.router.navigate(['/onboarding/add-to-cart'])
-    }else {
-      this.router.navigate(['/onboarding/login'],{replaceUrl:true,skipLocationChange:true})
+    } else {
+      this.router.navigate(['/onboarding/login'], { replaceUrl: true, skipLocationChange: true })
     }
   }
 
+  ngOnDestroy(): void {
+    this.navigationSubs.unsubscribe();
+  }
 }
