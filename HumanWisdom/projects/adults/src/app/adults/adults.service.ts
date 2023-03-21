345b74d135e27feaf810871c3ef7a6d1b8efc522
betsyjohn0112 <betsyjohn0112@gmail.com>
2023-03-21 15:16:55 +0530

    Issue for logged in Guest credentials after accessing modules
    Issue for logged in Guest credentials after accessing modules

--- HumanWisdom/projects/adults/src/app/adults/adults.service.ts
+++ HumanWisdom/projects/adults/src/app/adults/adults.service.ts
@@ -405,8 +405,8 @@ export class AdultsService {
     this.services.emailLogin(email, password)
       .subscribe(
         res => {
-          localStorage.setItem("isloggedin", 'T')
-          localStorage.setItem("remember", 'T')
+          // localStorage.setItem("isloggedin", 'T')
+          // localStorage.setItem("remember", 'T')
           loginResponse = res
           userId = res.UserId
           if (res.Subscriber === 0) {
