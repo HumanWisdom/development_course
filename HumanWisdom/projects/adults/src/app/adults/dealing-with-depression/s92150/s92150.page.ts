654669f6b9b02bd349e17ae19312199f276d94fe
betsyjohn0112 <betsyjohn0112@gmail.com>
2023-03-29 15:44:57 +0530

    resolved Issues with session end circle and tile Progress bar
    resolved Issues with session end circle and tile Progress bar

--- HumanWisdom/projects/adults/src/app/adults/dealing-with-depression/s92150/s92150.page.ts
+++ HumanWisdom/projects/adults/src/app/adults/dealing-with-depression/s92150/s92150.page.ts
@@ -44,7 +44,7 @@ export class S92150Page implements OnInit {
   {
     this.service.getPoints(this.userId)
     .subscribe(res=>{
-     this.progressPercent=parseInt(res.ModUserScrPc.find(e=>e.Module=="Dealing with depression").Percentage)
+     this.progressPercent=parseInt(res.ModUserScrPc.find(e=>e.Module=="Dealing with Depression").Percentage)
      console.log(this.progressPercent)
     })
   }
