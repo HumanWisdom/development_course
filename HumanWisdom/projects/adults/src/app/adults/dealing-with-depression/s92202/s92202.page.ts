c5e5404bafbc069e193ad0e645e1859125fc6947
Sana Sayyed <68653914+s29a9s93@users.noreply.github.com>
2023-03-29 23:51:20 +0530

    ui changes 1. Bookmarks - external approval 2. Video poster - external approval, dealing with depression 3. Screen background appearing for mobile device due to meta tag 4. 92143-nature meditation redirecting to without language module, should route nature module 5. 92181-remove click here, back link redirecting to dashboard it should go to same module 6. 92088t-the points given in audio file should be listed with bullets

--- HumanWisdom/projects/adults/src/app/adults/dealing-with-depression/s92202/s92202.page.ts
+++ HumanWisdom/projects/adults/src/app/adults/dealing-with-depression/s92202/s92202.page.ts
@@ -16,7 +16,7 @@ export class S92202Page implements OnInit,OnDestroy {
   mediaVideo=JSON.parse(localStorage.getItem("mediaVideo"))
   videoLink=this.mediaVideo+'/dealing-with-depression/videos/6.3.mp4'  
   title="You hold the keys to your own recovery"
-  poster="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/video_posters/comparison/comparison_01.jpg"
+  poster="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/video_posters/etc/08.jpg"
   
   userId:any
   saveUsername=JSON.parse(localStorage.getItem("saveUsername"))
