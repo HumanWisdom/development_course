a5a3b1f5e1e515396782f841cda459243cd174c3
tarini <tarini13trivedi@gmail.com>
2023-04-04 20:21:49 +0530

    resolved comments from sana

--- HumanWisdom/projects/teenagers/src/app/teenagers/benefits-of-enquiry/benefits-of-enquiry.module.ts
+++ HumanWisdom/projects/teenagers/src/app/teenagers/benefits-of-enquiry/benefits-of-enquiry.module.ts
@@ -5,8 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { SharedModule } from '../../../../../shared/shared.module';
 import { BenefitsOfEnquiryRoutingModule } from './benefits-of-enquiry-routing.module';
+import { TeenagersService } from '../teenagers.service';
 import { S95002Page } from './s95002/s95002.page';
-import { S95008Page } from './s95008/s95008.page';
 import { S95003Page } from './s95003/s95003.page';
 import { S95003tPage } from './s95003t/s95003t.page';
 import { S95004Page } from './s95004/s95004.page';
@@ -14,6 +14,8 @@ import { S95005Page } from './s95005/s95005.page';
 import { S95006Page } from './s95006/s95006.page';
 import { S95006tPage } from './s95006t/s95006t.page';
 import { S95007Page } from './s95007/s95007.page';
+import { S95008Page } from './s95008/s95008.page';
+import { S95009Page } from './s95009/s95009.page';
 import { S95010Page } from './s95010/s95010.page';
 import { S95010tPage } from './s95010t/s95010t.page';
 import { S95011Page } from './s95011/s95011.page';
@@ -30,23 +32,53 @@ import { S95018tPage } from './s95018t/s95018t.page';
 import { S95019Page } from './s95019/s95019.page';
 import { S95020Page } from './s95020/s95020.page';
 import { S95021Page } from './s95021/s95021.page';
-import { S95027Page } from './s95027/s95027.page';
-import { S95028Page } from './s95028/s95028.page';
-import { TeenagersService } from '../teenagers.service';
-import { S95009Page } from './s95009/s95009.page';
+import { S95022Page } from './s95022/s95022.page';
+import { S95023Page } from './s95023/s95023.page';
 import { S95024Page } from './s95024/s95024.page';
 import { S95025Page } from './s95025/s95025.page';
 import { S95026Page } from './s95026/s95026.page';
-import { S95022Page } from './s95022/s95022.page';
-import { S95023Page } from './s95023/s95023.page';
+import { S95027Page } from './s95027/s95027.page';
+import { S95028Page } from './s95028/s95028.page';
+
 
 
 
 @NgModule({
-  declarations: [S95001Page, S95002Page, S95003Page, S95003tPage, S95004Page, S95005Page, S95006Page, S95006tPage, S95007Page,
-    S95008Page,S95009Page, S95010Page, S95010tPage, S95011Page, S95012Page, S95013Page, S95013tPage, S95014Page,
-    S95015Page, S95016Page, S95016tPage, S95017Page, S95018Page, S95018tPage, S95019Page, S95020Page,
-    S95021Page, S95022Page,  S95023Page,  S95024Page,  S95025Page,  S95026Page,  S95027Page, S95028Page
+  declarations: [
+    S95001Page, 
+    S95002Page, 
+    S95003Page, 
+    S95003tPage, 
+    S95004Page, 
+    S95005Page, 
+    S95006Page, 
+    S95006tPage, 
+    S95007Page,
+    S95008Page,
+    S95009Page, 
+    S95010Page, 
+    S95010tPage, 
+    S95011Page, 
+    S95012Page, 
+    S95013Page, 
+    S95013tPage, 
+    S95014Page,
+    S95015Page, 
+    S95016Page, 
+    S95016tPage, 
+    S95017Page, 
+    S95018Page, 
+    S95018tPage, 
+    S95019Page, 
+    S95020Page,
+    S95021Page, 
+    S95022Page,  
+    S95023Page,  
+    S95024Page,  
+    S95025Page,  
+    S95026Page,  
+    S95027Page, 
+    S95028Page
   ],
   providers:[
     TeenagersService
