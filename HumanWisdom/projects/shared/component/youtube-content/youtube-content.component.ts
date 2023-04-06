ec3848231c613d989b0b2197548161897c19b03f
betsyjohn0112 <betsyjohn0112@gmail.com>
2023-04-06 14:30:52 +0530

    Resolved Events access Bug .. you tube links of other videos were not playing
    Resolved Events access Bug .. you tube links of other videos not playing

--- HumanWisdom/projects/shared/component/youtube-content/youtube-content.component.ts
+++ HumanWisdom/projects/shared/component/youtube-content/youtube-content.component.ts
@@ -10,15 +10,30 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class YoutubeContentComponent implements OnInit {
   public videoLink: any;
   public linkcode: any;
-  public access: string='free';
+ 
 
   @Input() bg: string;
   
   constructor(private route: ActivatedRoute,private _sanitizer: DomSanitizer, private router: Router) {
     this.linkcode = this.route.snapshot.paramMap.get('videolink')
-    this.access = this.route.snapshot.paramMap.get('access')
-    let sub: any = localStorage.getItem("Subscriber")
-    if (sub==0 && this.access=='paid' )
+   
+    let accesscode = 'rdtfghjhfdg'
+  
+   if(this.linkcode.includes('=')==true)
+   {  accesscode =this.linkcode.split('=')[1]
+     this.linkcode =this.linkcode.split('=')[0]
+     console.log(accesscode)
+  }
+
+
+  // let accesscode = this.linkcode.contains('=')==true?this.linkcode.splitarr('=')[1]:'rdtfghjhfdg'  
+   let access='free'
+   if (accesscode=='rdtfghjhfdg' ) access='free'
+   if (accesscode=='vncbxdfchgvxd' ) access='paid'
+
+
+    let sub = localStorage.getItem("Subscriber")
+    if (sub=='0' && access=='paid' )
       this.router.navigate(['/onboarding/free-limit']);
 
     if(window.history.state.class)
