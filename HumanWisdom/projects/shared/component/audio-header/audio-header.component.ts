bb90d23b34eaa5138cdc50acc4577e8ade4379c9
thisaiveerapandian <thisaiveerapandian.1995@gmail.com>
2023-05-09 23:28:49 +0530

    managed bookmark icon for guest user

--- HumanWisdom/projects/shared/component/audio-header/audio-header.component.ts
+++ HumanWisdom/projects/shared/component/audio-header/audio-header.component.ts
@@ -40,6 +40,7 @@ export class AudioHeaderComponent implements OnInit {
   placeHolder = 'Type your note here...';
   guest = false;
   Subscriber = false;
+  enableAlert = false;
 
   constructor(private router: Router,
     private service: AdultsService, public platform: Platform,
@@ -76,8 +77,12 @@ export class AudioHeaderComponent implements OnInit {
   }
 
   toggleBookmark() {
-    this.bookmark = !this.bookmark
-    this.sendBookmark.emit(this.bookmark)
+    if (this.guest || !this.Subscriber) {
+      this.enableAlert = true;
+    } else {
+      this.bookmark = !this.bookmark
+      this.sendBookmark.emit(this.bookmark)
+    }
   }
 
   addZero(i) {
@@ -200,5 +205,17 @@ export class AudioHeaderComponent implements OnInit {
         }
       )
 
+  }
+
+  getAlertcloseEvent(event) {
+    this.enableAlert = false;
+    if (event === 'ok') {
+      if (!this.guest && !this.Subscriber) {
+        this.router.navigate(["/onboarding/add-to-cart"]);
+      } else if (this.guest) {
+        localStorage.setItem("subscribepage", 'T');
+        this.router.navigate(["/onboarding/login"]);
+      }
+    }
   }
 }
