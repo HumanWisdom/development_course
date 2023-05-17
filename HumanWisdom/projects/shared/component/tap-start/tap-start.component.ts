494f40d5b656ae902fb909a6368597488ec6ed80
thisaiveerapandian <thisaiveerapandian.1995@gmail.com>
2023-05-17 20:39:02 +0530

    added continue to dashoard button in module start

--- HumanWisdom/projects/shared/component/tap-start/tap-start.component.ts
+++ HumanWisdom/projects/shared/component/tap-start/tap-start.component.ts
@@ -9,21 +9,23 @@ import { AdultsService } from '../../../adults/src/app/adults/adults.service';
   styleUrls: ['./tap-start.component.scss'],
 })
 export class TapStartComponent implements OnInit,AfterViewInit {
- 
+
   @Input() bg: string
   @Input() bg_tts: string;
   @Output() nextEmitter = new EventEmitter<string>();
 
   scrId:any
   path:any
-  
+
   @ViewChild('screen', { static: true }) screen: any;
 
   pageaction = localStorage.getItem("pageaction");
+  bg_cft="bg_dark_blue"
+
   constructor( private captureService:NgxCaptureService,
     private aservice:AdultsService,
-    private router:Router) { 
-      
+    private router:Router) {
+
     }
 
   ngOnInit() {
@@ -31,14 +33,14 @@ export class TapStartComponent implements OnInit,AfterViewInit {
     let str = this.path.replace(/\D/g,'');
     this.scrId = str;
     //this.scrId=this.scrId.parseInt()
-     
+
   }
   next(){
     this.nextEmitter.emit()
   }
   ngAfterViewInit(){
     // this.captureService.getImage(this.screen.nativeElement, true).toPromise().then(img=>{
-      
+
     //   img = img.substring(img.indexOf(",") + 1);
     //  // img.replace('data:image/png;base64,',' ')
     //   console.log(img)
@@ -47,7 +49,7 @@ export class TapStartComponent implements OnInit,AfterViewInit {
     //       console.log(r)
     //     }
     //   )
-      
+
     // })
 
 
