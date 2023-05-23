f648adce6c399b1064c6c05d750e38b27eb14904
ashwinirathod123 <86161974+ashwinirathod123@users.noreply.github.com>
2023-05-23 16:38:58 +0530

    video_poster

--- HumanWisdom/projects/teenagers/src/app/teenagers/questions-are-key/s102011/s102011.page.ts
+++ HumanWisdom/projects/teenagers/src/app/teenagers/questions-are-key/s102011/s102011.page.ts
@@ -18,7 +18,7 @@ export class S102011Page implements OnInit,OnDestroy {
   mediaVideo=JSON.parse(localStorage.getItem("mediaVideo"))
   videoLink=this.mediaVideo+'/questions-are-key/videos/1.1.mp4'  
   title="Questions are the key to discovering wisdom"
-  poster="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/video_posters/comparison/comparison_01.jpg"
+  poster="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/video_posters/art_of_enquiry/art_of_enquiry_01.jpg"
   userId:any
   saveUsername=JSON.parse(localStorage.getItem("saveUsername"))
   screenType=localStorage.getItem("video")
--- HumanWisdom/projects/teenagers/src/app/teenagers/without-language/s103017/s103017.page.ts
+++ HumanWisdom/projects/teenagers/src/app/teenagers/without-language/s103017/s103017.page.ts
@@ -18,7 +18,7 @@ export class S103017Page implements OnInit,OnDestroy {
   mediaVideo=JSON.parse(localStorage.getItem("mediaVideo"))
   videoLink=this.mediaVideo+'/videos/1.1.mp4'  
   title="Notice every thought that comes along, as you watch this video.Can you look and listen without language?"
-  poster="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/video_posters/comparison/comparison_01.jpg"
+  poster="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/video_posters/art_of_enquiry/art_of_enquiry_02 .jpg"
   userId:any
   saveUsername=JSON.parse(localStorage.getItem("saveUsername"))
   screenType=localStorage.getItem("video")
