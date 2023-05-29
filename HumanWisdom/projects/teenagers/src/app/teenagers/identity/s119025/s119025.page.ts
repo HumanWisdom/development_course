ff48b7a1cbd2a13a8800b8daca9a17154c2b3364
ashwinirathod123 <86161974+ashwinirathod123@users.noreply.github.com>
2023-05-29 22:33:28 +0530

    identity_feedback_survey

--- HumanWisdom/projects/teenagers/src/app/teenagers/identity/s119025/s119025.page.ts
+++ HumanWisdom/projects/teenagers/src/app/teenagers/identity/s119025/s119025.page.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { TeenagersService } from '../../teenagers.service';
 
 @Component({
   selector: 'app-s119025',
   templateUrl: './s119025.page.html',
   styleUrls: ['./s119025.page.scss'],
 })
-export class S119025Page implements OnInit {
+export class S119025Page implements OnInit 
+{
+  bg_tn="bg_dark_blue"
+  bg_cft="bg_dark_blue"
+  bg="dark_blue_w6"
+  saveUsername=JSON.parse(localStorage.getItem("saveUsername"))
+  userId:any
+  userName:any
+  progressPercent:any
+  progressText="1/3"
+  link="/identity/s119025"
+  name="#2 Four ways identity impacts our life"
+  progressImg=""
+  toc="identity/s119001"
 
-  constructor() { }
+  constructor
+  (
+    private router: Router, 
+    private location:Location,
+    private service: TeenagersService
+  ) 
+  { }
 
-  ngOnInit() {
+  ngOnInit() 
+  {
+    if(this.saveUsername==false)
+    {
+      this.userId=JSON.parse(sessionStorage.getItem("userId"))
+      this.userName=JSON.parse(sessionStorage.getItem("userName"))
+    }
+    else
+    {
+      this.userId=JSON.parse(localStorage.getItem("userId"))
+      this.userName=JSON.parse(localStorage.getItem("userName"))
+    }
+    this.getProgress()
   }
 
+  getProgress()
+  {
+    this.service.getPoints(this.userId)
+    .subscribe(res=>{
+     this.progressPercent=parseInt(res.ModUserScrPc.find(e=>e.Module=="identity").Percentage)
+     console.log(this.progressPercent)
+    })
+  }
 }
