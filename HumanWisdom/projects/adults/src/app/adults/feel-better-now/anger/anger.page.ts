bd906322beec70c744aa616e35145a9453754258
iamsanaasayyed <iamsanaasayyed@gmail.com>
2023-07-18 21:29:46 +0530

    ui changes
    feel better now - anger build issue - journalclick

--- HumanWisdom/projects/adults/src/app/adults/feel-better-now/anger/anger.page.ts
+++ HumanWisdom/projects/adults/src/app/adults/feel-better-now/anger/anger.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-anger',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AngerPage implements OnInit {
 
+  @ViewChild('enablepopup') enablepopup: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getclcickevent(event) {
+    if (event === 'enablepopup') {
+      this.enablepopup.nativeElement.click();
+    }
   }
 
 }
