0859806ac57e2b8aba165c02079db4d2da0a8ac6
PavishJain21 <jainpavish@gmail.com>
2023-07-19 23:07:32 +0530

    added audio for feeling upset

--- HumanWisdom/projects/adults/src/app/adults/feel-better-now/feeling-upset/feeling-upset-routing.module.ts
+++ HumanWisdom/projects/adults/src/app/adults/feel-better-now/feeling-upset/feeling-upset-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FeelingUpsetPage } from './feeling-upset.page';
+import { SingleAudioContentComponent } from '../../../../../../shared/component/single-audio-content/single-audio-content.component';
 
 const routes: Routes = [
   {
     path: '',
     component: FeelingUpsetPage
-  }
+  },
+  {
+    path: 'audiopage/:audiolink/:title/:RowId',
+    component: SingleAudioContentComponent
+  },
 ];
 
 @NgModule({
--- HumanWisdom/projects/adults/src/app/adults/feel-better-now/feeling-upset/feeling-upset.page.ts
+++ HumanWisdom/projects/adults/src/app/adults/feel-better-now/feeling-upset/feeling-upset.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-feeling-upset',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feeling-upset.page.scss'],
 })
 export class FeelingUpsetPage implements OnInit {
-
-  constructor() { }
+  audioData:any;
+  constructor(private router: Router) { 
+    this.audioData={
+      url:'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/guided-meditation/audios/guided-meditation+1.22.mp3'
+    }
+  }
 
   ngOnInit() {
   }
-
+  audioevent(url) {
+    this.router.navigate(['feel-better-now/feeling-upset/audiopage/', url ,"Manage Expectations",Math.random() ])
+}
 }
