44bb9b25732defe3a97dad406bdd7916a1ebf88f
PavishJain21 <jainpavish@gmail.com>
2023-07-20 17:41:57 +0530

    changes done for event

--- HumanWisdom/projects/adults/src/app/adults/feel-better-now/feeling-upset/feeling-upset.page.ts
+++ HumanWisdom/projects/adults/src/app/adults/feel-better-now/feeling-upset/feeling-upset.page.ts
@@ -7,21 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feeling-upset.page.scss'],
 })
 export class FeelingUpsetPage implements OnInit {
-  audioData:any;
-  constructor(private router: Router) { 
-    this.audioData={
-      url:'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/guided-meditation/audios/guided-meditation+1.22.mp3'
+  mediaUrl: any;
+  constructor(private router: Router) {
+    this.mediaUrl = {
+      url: 'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/guided-meditation/audios/guided-meditation+1.22.mp3',
+      youtubeUrl: 'b5PZ6fFCL3g'
     }
   }
 
   ngOnInit() {
-    this.audioData={
-      url:'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/guided-meditation/audios/guided-meditation+1.22.mp3'
-    }
   }
 
-  audioevent(url) 
-  {
-      this.router.navigate(['feel-better-now/feeling-upset/audiopage/', url ,"Manage expectations",Math.random() ])
+  audioevent(url) {
+    this.router.navigate(['feel-better-now/feeling-upset/audiopage/', url, "Manage expectations", Math.random()])
+  }
+
+  routeToYoutube(url) {
+    this.router.navigate(['feel-better-now/feeling-upset/youtubelink/',url]);
   }
 }
