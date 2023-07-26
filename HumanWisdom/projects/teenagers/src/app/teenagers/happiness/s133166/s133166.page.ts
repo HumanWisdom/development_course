367a299458854b20722f2618db1a8a4738c8dd9f
tarini13 <tarini13trivedi@gmail.com>
2023-07-27 00:45:04 +0530

    commt

--- HumanWisdom/projects/teenagers/src/app/teenagers/happiness/s133166/s133166.page.ts
+++ HumanWisdom/projects/teenagers/src/app/teenagers/happiness/s133166/s133166.page.ts
@@ -13,7 +13,7 @@ export class S133166Page implements OnInit,OnDestroy {
   bg_tn="bg_red_pink"
   bg_cft="bg_red_pink"
   bg="red_pink_w10"
-  mediaVideo=JSON.parse(localStorage.getItem("mediaVideo"))
+  mediaVideo='https://humanwisdoms3.s3.eu-west-2.amazonaws.com'
   videoLink=this.mediaVideo+'/happiness/videos/5.2.mp4'  
   title="How can a positive attitude help me be happy?"
   poster="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/images/tiles/video_posters/happiness/happiness_03.jpg"
