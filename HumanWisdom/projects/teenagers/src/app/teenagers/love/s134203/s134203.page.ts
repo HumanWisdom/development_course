15ab05d272329b0adbfd88306cf253991a3d2900
ashwinirathod123 <86161974+ashwinirathod123@users.noreply.github.com>
2023-09-16 13:20:23 +0530

    anger_module_and_other_changes

--- HumanWisdom/projects/teenagers/src/app/teenagers/love/s134203/s134203.page.ts
+++ HumanWisdom/projects/teenagers/src/app/teenagers/love/s134203/s134203.page.ts
@@ -74,7 +74,7 @@ export class S134203Page implements OnInit {
     if(this.optionList && this.sessionOption134203) {
       this.optionList.forEach((d) => {
         if(this.sessionOption134203.includes(d['OptId'])) {
-          document.getElementById(d['OptStr']).style.backgroundColor = '#FFC455';
+        document.getElementById(d['OptStr']).style.backgroundColor = '#E58D82';
         }
       }) 
     }   
@@ -128,12 +128,12 @@ export class S134203Page implements OnInit {
    console.log(id,e)
    if(e==true)
    {
-    document.getElementById(divid).style.backgroundColor = '#FFC455';
+    document.getElementById(divid).style.backgroundColor = '#E58D82';
      this.sendOption.push(id)
    }
    else if(e==false)
    {
-    document.getElementById(divid).style.backgroundColor = 'rgba(255,255,255,0.75)';
+  document.getElementById(divid).style.backgroundColor = 'rgba(255,255,255,0.1)';
     this.sendOption.forEach((element,index)=>{
       if(element==id) this.sendOption.splice(index,1);
    });
@@ -174,7 +174,7 @@ export class S134203Page implements OnInit {
   sessionFetch(id, divid){
     if(this.sessionOption134203.includes(id))
     {
-      // document.getElementById(divid).style.backgroundColor = '#FFC455';
+      // document.getElementById(divid).style.backgroundColor = '#E58D82';
       return true
     }
 
--- HumanWisdom/projects/teenagers/src/app/teenagers/success-failure/s141025/s141025.page.ts
+++ HumanWisdom/projects/teenagers/src/app/teenagers/success-failure/s141025/s141025.page.ts
@@ -80,7 +80,7 @@ export class S141025Page implements OnInit {
     if(this.optionList && this.sessionOption141025) {
       this.optionList.forEach((d) => {
         if(this.sessionOption141025.includes(d['OptId'])) {
-          document.getElementById(d['OptStr']).style.backgroundColor = '#FFC455';
+        document.getElementById(d['OptStr']).style.backgroundColor = '#E58D82';
         }
       }) 
     }   
@@ -134,12 +134,12 @@ export class S141025Page implements OnInit {
    console.log(id,e)
    if(e==true)
    {
-    document.getElementById(divid).style.backgroundColor = '#FFC455';
+    document.getElementById(divid).style.backgroundColor = '#E58D82';
      this.sendOption.push(id)
    }
    else if(e==false)
    {
-    document.getElementById(divid).style.backgroundColor = 'rgba(255,255,255,0.75)';
+  document.getElementById(divid).style.backgroundColor = 'rgba(255,255,255,0.1)';
     this.sendOption.forEach((element,index)=>{
       if(element==id) this.sendOption.splice(index,1);
    });
@@ -182,7 +182,7 @@ export class S141025Page implements OnInit {
   sessionFetch(id, divid){
     if(this.sessionOption141025.includes(id))
     {
-      // document.getElementById(divid).style.backgroundColor = '#FFC455';
+      // document.getElementById(divid).style.backgroundColor = '#E58D82';
       return true
     }
 
