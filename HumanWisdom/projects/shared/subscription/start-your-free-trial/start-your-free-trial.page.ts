ed0abc0022e4e4ec50583bbd949d185a9ac7101c
PavishJain21 <jainpavish@gmail.com>
2023-09-21 20:55:32 +0530

    added basck link

--- HumanWisdom/projects/shared/subscription/start-your-free-trial/start-your-free-trial.page.ts
+++ HumanWisdom/projects/shared/subscription/start-your-free-trial/start-your-free-trial.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-start-your-free-trial',
   templateUrl: './start-your-free-trial.page.html',
@@ -8,13 +8,17 @@ import { Router } from '@angular/router';
 })
 export class StartYourFreeTrialPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,    private location: Location) { }
 
   ngOnInit() {
   }
 
   tryFreeSubscribe(){
     this.router.navigate(['/subscription/try-free-and-subscribe']);
+  }
+
+  back(){
+    this.location.back();
   }
 
   routeToDashboard(){
