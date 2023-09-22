0dddfa140418c4497d34bd5c950bed2a0ba1843a
betsyjohn0112 <betsyjohn0112@gmail.com>
2023-09-22 19:46:26 +0530

    Diagram  Audio and  transcript Files completed and resolved
    Diagram  Audio and  transcript Files completed and resolved

--- HumanWisdom/projects/teenagers/src/app/teenagers/food-health/s128010t/s128010t.page.ts
+++ HumanWisdom/projects/teenagers/src/app/teenagers/food-health/s128010t/s128010t.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TeenagersService } from '../../teenagers.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-s128010t',
@@ -6,10 +10,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./s128010t.page.scss'],
 })
 export class S128010tPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  bg_tn="bg_green"
+  bg_cft="bg_green"
+  bg="green_w1"
+  bookmark=0
+  path = setTimeout(() => {
+    return this.router.url;
+  }, 1000);
+  audioPage="/food-health/s128010"
+  toc="/food-health/s128001"
+  screenType=localStorage.getItem("audio")
+  userId:any
+  moduleId=localStorage.getItem("moduleId")
+  screenNumber=128010
+  startTime:any
+  endTime:any
+  saveUsername=JSON.parse(localStorage.getItem("saveUsername"))
+  avDuration=localStorage.getItem("avDuration128010")
+  totalTime=localStorage.getItem("totalTime128010")
+  bookmarkList=JSON.parse(localStorage.getItem("bookmarkList"))
+  progName = "teenagers"
+  
+  constructor
+  (
+    private router: Router,
+    private service:TeenagersService,
+    private location:Location
+  ) 
+  { }
+ 
+  ngOnInit() 
+  {
+    if(this.saveUsername==false)
+    {
+      this.userId=JSON.parse(sessionStorage.getItem("userId"))
+    }
+    else
+    {
+      this.userId=JSON.parse(localStorage.getItem("userId"))
+    }
+    if(JSON.parse(sessionStorage.getItem("bookmark128010"))==0)
+    this.bookmark=0
+    else if(this.bookmarkList.includes(this.screenNumber)||JSON.parse(sessionStorage.getItem("bookmark128010"))==1)
+      this.bookmark=1
   }
+
+  receiveBookmark(e)
+  {
+    console.log(e)
+    if(e==true)
+      this.bookmark=1
+    else
+      this.bookmark=0
+    sessionStorage.setItem("bookmark128010",JSON.stringify(this.bookmark))
+  }
+
+  submitProgress()
+  {
+    this.router.navigate(['/food-health/s128011'])
+    this.service.submitProgressAv({
+      "ScrNumber":this.screenNumber,
+      "UserId":this.userId,
+      "BookMark":this.bookmark,
+      "ModuleId":this.moduleId,
+      "screenType":this.screenType,
+      "timeSpent":this.totalTime,
+      "avDuration":this.avDuration
+    }).subscribe(res=>{})
+  }
+
+  prev()
+  {
+    this.router.navigate(['/food-health/s128009'])
+  }
+
 
 }
