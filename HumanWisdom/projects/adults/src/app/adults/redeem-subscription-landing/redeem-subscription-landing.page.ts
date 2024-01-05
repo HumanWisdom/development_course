9a7c26a62f563fc0a89a09621cfb1fcc2244e055
iamsanaasayyed <iamsanaasayyed@gmail.com>
2024-01-05 12:28:05 +0530

    ui changes
    1. redeem subscription landing

---
+++ HumanWisdom/projects/adults/src/app/adults/redeem-subscription-landing/redeem-subscription-landing.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redeem-subscription-landing',
+  templateUrl: './redeem-subscription-landing.page.html',
+  styleUrls: ['./redeem-subscription-landing.page.scss'],
+})
+export class RedeemSubscriptionLandingPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
