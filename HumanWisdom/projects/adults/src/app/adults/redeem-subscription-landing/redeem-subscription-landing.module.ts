14efc5835fbb8d500561ec4968928e4d4cc27dc2
thisaiveerapandian <thisaiveerapandian.1995@gmail.com>
2024-01-10 11:06:58 +0530

    code changes

--- HumanWisdom/projects/adults/src/app/adults/redeem-subscription-landing/redeem-subscription-landing.module.ts
+++ HumanWisdom/projects/adults/src/app/adults/redeem-subscription-landing/redeem-subscription-landing.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { RedeemSubscriptionLandingPageRoutingModule } from './redeem-subscription-landing-routing.module';
 
 import { RedeemSubscriptionLandingPage } from './redeem-subscription-landing.page';
+import { SharedModule } from '../../../../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RedeemSubscriptionLandingPageRoutingModule
+    RedeemSubscriptionLandingPageRoutingModule,
+    SharedModule
   ],
   declarations: [RedeemSubscriptionLandingPage]
 })
