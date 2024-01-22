27a2c789cb7892b07c1c58fff6969a1b480a9671
PavishJain21 <jainpavish@gmail.com>
2024-01-22 22:24:49 +0530

    added design changes

--- HumanWisdom/projects/adults/src/app/app.component.ts
+++ HumanWisdom/projects/adults/src/app/app.component.ts
@@ -37,6 +37,7 @@ export class AppComponent implements OnDestroy {
   public pageLoaded = false;
   navigationSubs = new Subscription();
   dash = false;
+  adultsCss='assets/css/custom.css'
   programType: ProgramType = ProgramType.Adults;
   journal = false
   fourm = false
@@ -95,6 +96,7 @@ export class AppComponent implements OnDestroy {
 
     this.initializeApp();
     this.getFreeScreens();
+    // this.setDynamicCSS();
     // if (window.matchMedia('(display-mode: standalone)').matches) {
     //   localStorage.setItem("isPWA", 'APP')
     // } else {
@@ -103,7 +105,7 @@ export class AppComponent implements OnDestroy {
   }
 
   prepareRoute(outlet: RouterOutlet) {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation']
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
 
 
@@ -448,5 +450,8 @@ export class AppComponent implements OnDestroy {
     SharedService.enablebanner = false
   }
 
+  setDynamicCSS(){
+     //   window.document.getElementById('adultsCss').setAttribute('href',this.adultsCss);
+ }
 }
 
