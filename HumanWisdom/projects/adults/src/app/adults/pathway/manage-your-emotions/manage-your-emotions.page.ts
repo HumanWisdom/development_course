f953d15918da8a1588f9c704f79c2eb1287c44d2
betsyjohn0112 <betsyjohn0112@gmail.com>
2024-02-22 13:43:41 +0530

    Minor changes
    Event tracking added to free trial , Pathway module click, testimonial page
    Resolved length validation of event registration,
    Forum thread Page minor bugs resolved

--- HumanWisdom/projects/adults/src/app/adults/pathway/manage-your-emotions/manage-your-emotions.page.ts
+++ HumanWisdom/projects/adults/src/app/adults/pathway/manage-your-emotions/manage-your-emotions.page.ts
@@ -5,6 +5,7 @@ import { Location } from '@angular/common';
 import { SharedService } from '../../../../../../shared/services/shared.service';  
 import { Constant } from '../../../../../../shared/services/constant'; 
 import { NavigationService } from '../../../../../../shared/services/navigation.service';
+import { LogEventService } from '../../../../../../shared/services/log-event.service'; 
 
 @Component({
   selector: 'app-manage-your-emotions',
@@ -15,9 +16,12 @@ export class ManageYourEmotionsPage implements OnInit {
 
   @ViewChild('enablepopup') enablepopup: ElementRef;
 
-  constructor(public router: Router, public service: AdultsService, private location: Location,private navigationService: NavigationService) { }
+  constructor(public router: Router, public service: AdultsService,
+     private location: Location,     public logeventservice: LogEventService,
+     private navigationService: NavigationService) { }
 
   ngOnInit() {
+    this.logeventservice.logEvent('view_manage-your-emotions');
     SharedService.setDataInLocalStorage(Constant.NaviagtedFrom, this.router.url);
 
   }
