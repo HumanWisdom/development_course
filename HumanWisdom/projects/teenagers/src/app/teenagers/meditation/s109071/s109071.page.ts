3e208e5210ad42cf947d578e2ac5b23c2e99e954
tarini13 <tarini13trivedi@gmail.com>
2024-02-23 12:34:51 +0530

    commit for bugs

--- HumanWisdom/projects/teenagers/src/app/teenagers/meditation/s109071/s109071.page.ts
+++ HumanWisdom/projects/teenagers/src/app/teenagers/meditation/s109071/s109071.page.ts
@@ -12,7 +12,7 @@ export class S109071Page implements OnInit, OnDestroy
 {
   bg_tn="bg_blue"
   bg_cft="bg_blue"
-  bg="blue_flat"
+  bg="blue_w1"
   toc="meditation/s109001"
   userId: any
   saveUsername = JSON.parse(localStorage.getItem("saveUsername"))
