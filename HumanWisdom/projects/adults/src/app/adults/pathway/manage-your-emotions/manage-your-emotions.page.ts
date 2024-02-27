91bf4e452a03f4691de4a03e109c2872a959c779
betsyjohn0112 <betsyjohn0112@gmail.com>
2024-02-27 20:16:16 +0530

    Code replication for Progress bar in all pathway pages
    Code replication for Progress bar in all pathway pages

--- HumanWisdom/projects/adults/src/app/adults/pathway/manage-your-emotions/manage-your-emotions.page.ts
+++ HumanWisdom/projects/adults/src/app/adults/pathway/manage-your-emotions/manage-your-emotions.page.ts
@@ -15,12 +15,30 @@ import { LogEventService } from '../../../../../../shared/services/log-event.ser
 export class ManageYourEmotionsPage implements OnInit {
 
   @ViewChild('enablepopup') enablepopup: ElementRef;
+  public anxietyP: any
+  public depressionP: any
+  public pleasureP: any
+  public sorrowP: any
+  public lonelinessP: any
+  public angerP: any
+  public deathP: any
+
 
   constructor(public router: Router, public service: AdultsService,
      private location: Location,     public logeventservice: LogEventService,
      private navigationService: NavigationService) { }
 
   ngOnInit() {
+    let userId = JSON.parse(localStorage.getItem("userId")) ? JSON.parse(localStorage.getItem("userId")) : 100;
+    this.service.getPoints(userId).subscribe((d) => {
+      this.anxietyP = d['ModUserScrPc'].find(e => e.ModuleId  == 19)?.Percentage;
+      this.depressionP = d['ModUserScrPc'].find(e => e.ModuleId == 92)?.Percentage;
+      this.pleasureP = d['ModUserScrPc'].find(e => e.ModuleId == 20)?.Percentage;
+      this.sorrowP = d['ModUserScrPc'].find(e => e.ModuleId == 60)?.Percentage;
+      this.lonelinessP = d['ModUserScrPc'].find(e => e.ModuleId == 61)?.Percentage;
+      this.angerP = d['ModUserScrPc'].find(e => e.ModuleId == 14)?.Percentage;
+      this.deathP = d['ModUserScrPc'].find(e => e.ModuleId == 64)?.Percentage;
+    });
     this.logeventservice.logEvent('view_manage-your-emotions');
     SharedService.setDataInLocalStorage(Constant.NaviagtedFrom, this.router.url);
 
