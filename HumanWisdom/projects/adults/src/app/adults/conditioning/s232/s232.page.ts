236e09b7d73360d98b022e34714bbaf90fb9d01b
thisaiveerapandian <thisaiveerapandian.1995@gmail.com>
2024-03-01 10:46:46 +0530

    code changes

--- HumanWisdom/projects/adults/src/app/adults/conditioning/s232/s232.page.ts
+++ HumanWisdom/projects/adults/src/app/adults/conditioning/s232/s232.page.ts
@@ -1,38 +1,38 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import {AdultsService} from "../../adults.service"
-import { Router,ActivatedRoute } from '@angular/router';
-import {Location } from '@angular/common'
+import { AdultsService } from "../../adults.service"
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-s232',
   templateUrl: './s232.page.html',
   styleUrls: ['./s232.page.scss'],
 })
-export class S232Page implements OnInit,OnDestroy {
-  userId:any
-  saveUsername=JSON.parse(localStorage.getItem("saveUsername"))
-  screenType=localStorage.getItem("text")
-  moduleId=localStorage.getItem("moduleId")
-  screenNumber=232
-  startTime:any
-  endTime:any
-  totalTime:any
-  bookmark:any
-  bookmarkList=[]
+export class S232Page implements OnInit, OnDestroy {
+  userId: any
+  saveUsername = JSON.parse(localStorage.getItem("saveUsername"))
+  screenType = localStorage.getItem("text")
+  moduleId = localStorage.getItem("moduleId")
+  screenNumber = 232
+  startTime: any
+  endTime: any
+  totalTime: any
+  bookmark: any
+  bookmarkList = []
   path = setTimeout(() => {
     return this.router.url;
   }, 1000);
-  
- 
-  token="1234"
-  shareUrl=this.path+"?t="+this.token
-  freeScreens=JSON.parse(localStorage.getItem("freeScreens"))
-  socialShare=false
-  loginResponse=JSON.parse(localStorage.getItem("loginResponse"))
-  t:any
-  conditioningResume=sessionStorage.getItem("pgResume")
-  tocImage="https://d1tenzemoxuh75.cloudfront.net/assets/images/background/toc/15.webp"
-  tocColor="white"
+
+
+  token = "1234"
+  shareUrl = this.path + "?t=" + this.token
+  freeScreens = JSON.parse(localStorage.getItem("freeScreens"))
+  socialShare = false
+  loginResponse = JSON.parse(localStorage.getItem("loginResponse"))
+  t: any
+  conditioningResume = sessionStorage.getItem("pgResume")
+  tocImage = "https://d1tenzemoxuh75.cloudfront.net/assets/images/background/toc/15.webp"
+  tocColor = "white"
   lastvisited = false;
   stories: any = []
   isLoggedIn = false;
@@ -40,47 +40,18 @@ export class S232Page implements OnInit,OnDestroy {
 
   constructor(
     private router: Router,
-    private service:AdultsService,
-    private location:Location,
+    private service: AdultsService,
+    private location: Location,
     private url: ActivatedRoute
-  ) { 
+  ) {
     this.service.setmoduleID(15);
-      this.url.queryParams.subscribe(params => {
+    this.url.queryParams.subscribe(params => {
       this.t = params['t'];
     })
 
-    let story = JSON.parse(JSON.stringify(localStorage.getItem('wisdomstories')));
-    story = JSON.parse(story)
-    let splitarr = []
-    let arraythree = []
-    if(story?.length <= 2) 
-    {
-      story.forEach((e) => 
-      {
-        arraythree.push(e)
-      })
-      splitarr.push(arraythree)
-    }
-    else
-    {
-      story?.forEach((e) => 
-      {
-        if(arraythree.length < 2) 
-        {
-          arraythree.push(e)
-        }
-        else 
-        {
-          splitarr.push(arraythree)
-          arraythree = []
-          arraythree.push(e)
-        }
-      })
-    }
-    this.stories = splitarr
     // this.stories = JSON.parse(JSON.stringify(localStorage.getItem('wisdomstories')));
     // this.stories = JSON.parse(this.stories)
-     
+
   }
 
   ngOnInit() {
@@ -91,106 +62,127 @@ export class S232Page implements OnInit,OnDestroy {
       this.isSubscriber = true;
     }
 
-    
-    if(!localStorage.getItem("NaviagtedFrom"))  
-    localStorage.setItem("NaviagtedFrom", '/adults/pathway/understand-how-your-mind-works');
 
-    // continue where you left    
+    if (!localStorage.getItem("NaviagtedFrom"))
+      localStorage.setItem("NaviagtedFrom", '/adults/pathway/understand-how-your-mind-works');
+
+    // continue where you left
     let last = localStorage.getItem('lastvisited');
-    if(last === 'T') 
-    {
+    if (last === 'T') {
       this.lastvisited = true;
     }
-    else 
-    {
+    else {
       this.lastvisited = false;
-    }    
+    }
     // /continue where you left
 
-    console.log(this.shareUrl,this.loginResponse)
-    
-    if(this.saveUsername==false)
-      {this.userId=JSON.parse(sessionStorage.getItem("userId"))}
-  else
-    {this.userId=JSON.parse(localStorage.getItem("userId"))}
+    console.log(this.shareUrl, this.loginResponse)
 
-    if(!this.t) //if no token in url- not shared
+    if (this.saveUsername == false) { this.userId = JSON.parse(sessionStorage.getItem("userId")) }
+    else { this.userId = JSON.parse(localStorage.getItem("userId")) }
+
+    if (!this.t) //if no token in url- not shared
     {
-     
-      if(this.loginResponse.Subscriber!=1 && !this.freeScreens.includes(this.screenNumber))
+
+      if (this.loginResponse.Subscriber != 1 && !this.freeScreens.includes(this.screenNumber))
         console.log("move")
 
     }
-    else{
+    else {
       console.log("show")
 
     }
-     
-   
+
+
     this.startTime = Date.now();
-  
+
     this.startTime = Date.now();
+
+    setTimeout(() => {
+      let story = JSON.parse(JSON.stringify(localStorage.getItem('wisdomstories')));
+      story = JSON.parse(story)
+      let splitarr = []
+      let arraythree = []
+      if (story?.length <= 2) {
+        story.forEach((e) => {
+          arraythree.push(e)
+        })
+        splitarr.push(arraythree)
+      }
+      else {
+        story?.forEach((e) => {
+          if (arraythree.length < 2) {
+            arraythree.push(e)
+          }
+          else {
+            splitarr.push(arraythree)
+            arraythree = []
+            arraythree.push(e)
+          }
+        })
+      }
+      this.stories = splitarr
+    }, 2000)
+
     this.createScreen()
-    
-    
 
 
-    
+
+
+
   }
-  addToken(){
-    history.replaceState(null, null, this.path+`?t=${this.token}`);
-    this.socialShare=true
+  addToken() {
+    history.replaceState(null, null, this.path + `?t=${this.token}`);
+    this.socialShare = true
   }
-  toggleBookmark(){
-    if(this.bookmark==0)
-      this.bookmark=1
+  toggleBookmark() {
+    if (this.bookmark == 0)
+      this.bookmark = 1
     else
-      this.bookmark=0
+      this.bookmark = 0
 
   }
-  createScreen(){
+  createScreen() {
     this.service.createScreen({
-      "ScrId":0,
-      "ModuleId":this.moduleId,
-      "GSetID":this.screenType,
-      "ScreenNo":this.screenNumber
-    }).subscribe(res=>
-      {
-        
-      })
-    
+      "ScrId": 0,
+      "ModuleId": this.moduleId,
+      "GSetID": this.screenType,
+      "ScreenNo": this.screenNumber
+    }).subscribe(res => {
+
+    })
+
 
   }
-  goBack(){
+  goBack() {
     this.location.back()
   }
 
 
-  submitProgress(){
+  submitProgress() {
     this.service.submitProgressText({
-      "ScrNumber":this.screenNumber,
-      "UserId":this.userId,
-      "BookMark":this.bookmark,
-      "ModuleId":this.moduleId,
-      "screenType":this.screenType,
-      "timeSpent":this.totalTime
-    }).subscribe(res=>
-      {
-        
-        this.bookmarkList=res.GetBkMrkScr.map(a=>parseInt(a.ScrNo))
-        
-      })
-    
+      "ScrNumber": this.screenNumber,
+      "UserId": this.userId,
+      "BookMark": this.bookmark,
+      "ModuleId": this.moduleId,
+      "screenType": this.screenType,
+      "timeSpent": this.totalTime
+    }).subscribe(res => {
 
-  }
-  ngOnDestroy(){
-   
+      this.bookmarkList = res.GetBkMrkScr.map(a => parseInt(a.ScrNo))
 
+    })
 
 
   }
+  ngOnDestroy() {
 
-  routeJournal(){
+
+
+
+  }
+
+  routeJournal() {
     this.router.navigate(['/adults/journal'])
   }
 
