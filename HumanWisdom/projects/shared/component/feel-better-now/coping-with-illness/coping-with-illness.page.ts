8137be5f2dd3abb4e4239360ba0c4cc08610748f
tarini13 <tarini13trivedi@gmail.com>
2024-03-08 15:10:58 +0530

    commit for feel better soon

---
+++ HumanWisdom/projects/shared/component/feel-better-now/coping-with-illness/coping-with-illness.page.ts
@@ -0,0 +1,58 @@
+import { Router } from '@angular/router';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-coping-with-illness',
+  templateUrl: './coping-with-illness.page.html',
+  styleUrls: ['./coping-with-illness.page.scss'],
+})
+export class CopingWithIllnessPage implements OnInit {
+
+  @ViewChild('enablepopup') enablepopup: ElementRef;
+  mediaAudio=JSON.parse(localStorage.getItem("mediaAudio"))
+
+  mediaUrl:any;
+
+  constructor(private router: Router, private location: Location)
+  {
+    this.mediaUrl = {
+      url: 'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/guided-meditation/audios/guided-meditation+1.30.mp3',
+      youtubeUrl: 'tsl5QK9aqTI'
+    }
+  }
+
+  ngOnInit() {}
+
+  audioevent(url) {
+      this.router.navigate(['feel-better-now/coping-with-illness/audiopage/', url ,"Dealing with an illness",Math.random() ])
+  }
+
+  routeToYoutube(url) {
+    this.router.navigate(['feel-better-now/feeling-upset/youtubelink/',url]);
+  }
+
+  getclcickevent(event) {
+    if (event === 'enablepopup') {
+      this.enablepopup.nativeElement.click();
+    }
+  }
+
+  goBack() {
+    // this.location.back()
+    if (window.location.href.includes('teenagers')) {
+      this.router.navigate(['/feel-better-now']);
+    } else {
+      this.router.navigate(['/adults/feel-better-now']);
+    }
+  }
+
+  routeVideoaudio(type, url, title = '') {
+    if(type === 'video') {
+     this.router.navigate([url, 'F', title])
+    }else{
+      let concat = encodeURIComponent(url.replaceAll('/','~'));
+     this.router.navigate(['adults/audiopage/', concat, '1', 'F', title])
+    }
+ }
+}
---
+++ HumanWisdom/projects/shared/component/feel-better-now/depression/depression.page.ts
@@ -0,0 +1,58 @@
+import { Router } from '@angular/router';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-depression',
+  templateUrl: './depression.page.html',
+  styleUrls: ['./depression.page.scss'],
+})
+export class DepressionPage implements OnInit {
+
+  @ViewChild('enablepopup') enablepopup: ElementRef;
+  mediaAudio=JSON.parse(localStorage.getItem("mediaAudio"))
+
+  mediaUrl:any;
+
+  constructor(private router: Router, private location: Location)
+  {
+    this.mediaUrl = {
+      url: 'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/guided-meditation/audios/guided-meditation+1.31.mp3',
+      youtubeUrl: 'Liq_aj6jYd4'
+    }
+  }
+
+  ngOnInit() {}
+
+  audioevent(url) {
+      this.router.navigate(['feel-better-now/depression/audiopage/', url ,"Deal with depression",Math.random() ])
+  }
+
+  routeToYoutube(url) {
+    this.router.navigate(['feel-better-now/feeling-upset/youtubelink/',url]);
+  }
+
+  getclcickevent(event) {
+    if (event === 'enablepopup') {
+      this.enablepopup.nativeElement.click();
+    }
+  }
+
+  goBack() {
+    // this.location.back()
+    if (window.location.href.includes('teenagers')) {
+      this.router.navigate(['/feel-better-now']);
+    } else {
+      this.router.navigate(['/adults/feel-better-now']);
+    }
+  }
+
+  routeVideoaudio(type, url, title = '') {
+    if(type === 'video') {
+     this.router.navigate([url, 'F', title])
+    }else{
+      let concat = encodeURIComponent(url.replaceAll('/','~'));
+     this.router.navigate(['adults/audiopage/', concat, '1', 'F', title])
+    }
+ }
+}
