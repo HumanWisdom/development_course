93266ffad15f26016a94b8e26abaeec14638791e
PavishJain21 <jainpavish@gmail.com>
2024-03-23 15:49:50 +0530

    added custom events

--- HumanWisdom/projects/adults/src/app/app.component.ts
+++ HumanWisdom/projects/adults/src/app/app.component.ts
@@ -92,6 +92,7 @@ export class AppComponent implements OnDestroy {
     this.navigationSubs = this.router.events.pipe(
       filter((event) => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
+    //  this.navigationService.routeToPath(event.url);
       this.navigationService.addToHistory(event.url);
       this.services.previousUrl = this.services.currentUrl;
       this.services.currentUrl = event.url;
