66f0bc9dbd40f8e4c4d3fb9181543eb5fe81cc29
PavishJain21 <jainpavish@gmail.com>
2024-03-29 00:18:29 +0530

    fixed background color issue for life stories

--- HumanWisdom/projects/shared/component/submit-story/submit-story.page.ts
+++ HumanWisdom/projects/shared/component/submit-story/submit-story.page.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {Location } from '@angular/common'
-
+import { SharedService } from '../../services/shared.service';
+import { ProgramType } from '../../models/program-model';
 @Component({
   selector: 'app-submit-story',
   templateUrl: './submit-story.page.html',
   styleUrls: ['./submit-story.page.scss'],
 })
 export class SubmitStoryPage implements OnInit {
-
+  isAdults = true;
   constructor(private router: Router,
-    private location:Location) { }
+    private location:Location) { 
+
+      if (SharedService.ProgramId == ProgramType.Adults) {
+        this.isAdults = true;
+          } else {
+           this.isAdults = false;
+          }
+    }
 
   ngOnInit() {
   }
