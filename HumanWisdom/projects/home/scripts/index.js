7bb10ae30431e22f1f925fb5d2c8265b2ad908d0
PavishJain21 <jainpavish@gmail.com>
2024-04-05 19:28:43 +0530

    create splash screen in js project

--- HumanWisdom/projects/home/scripts/index.js
+++ HumanWisdom/projects/home/scripts/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    alert("JavaScript is working!");
+var adults = document.getElementById("adults");
+
+// Add a click event listener to the button
+adults.addEventListener("click", function() {
+    // This function will be executed when the button is clicked
+window.location.href = "https://staging.happierme.app/onboarding/login";
 });
+
+// Get the button element by its id
+var teenagers = document.getElementById("teenagers");
+// Add a click event listener to the button
+teenagers.addEventListener("click", function() {
+    // This function will be executed when the button is clicked
+window.location.href = "https://staging.happierme.app/teenagers/onboarding/login";
+});
+
