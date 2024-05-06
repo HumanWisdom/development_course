39fa704397577d14fa9685c6be4c5440425f2b3e
tarini13 <tarini13trivedi@gmail.com>
2024-05-06 15:35:51 +0530

    commit for carrer changes

---
+++ HumanWisdom/projects/teenagers/src/app/teenagers/career-success/s160083/s160083.page.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit ,ViewChild,  ElementRef, AfterViewInit,OnDestroy} from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import * as jQuery from 'jquery';
+import { TeenagersService } from '../../teenagers.service';
+@Component({
+  selector: 'app-s160083-audio',
+  templateUrl: './s160083.page.html',
+  styleUrls: ['./s160083.page.scss'],
+})
+
+export class S160083Page implements OnInit,OnDestroy 
+{
+  bg_tn=""
+  bg_cft=""
+  bg=""
+  title="#1 Develop your communications skills"
+  mediaAudio=JSON.parse(localStorage.getItem("mediaAudio"))
+  audioLink=this.mediaAudio+'/teenagers/modules/career-success/audios/1.5.mp3'
+  transcriptPage="career-success/s160083t"
+  toc="teenagers/career-success/s160001"
+  bookmark=0
+  path = setTimeout(() => {
+    return this.router.url;
+  }, 1000);
+  avDuration:any
+  userId:any
+  saveUsername=JSON.parse(localStorage.getItem("saveUsername"))
+  screenType=localStorage.getItem("audio")
+  moduleId=localStorage.getItem("moduleId")
+  screenNumber=160083
+  startTime:any
+  endTime:any
+  totalTime:any
+  bookmarkList=JSON.parse(localStorage.getItem("bookmarkList"))
+  progName ="teenagers"
+  
+  constructor
+  (
+    private router: Router,
+    private service:TeenagersService,
+    private location:Location
+  ) 
+  { }
+ 
+  ngOnInit() 
+  {
+    if(this.saveUsername==false)
+    {
+      this.userId=JSON.parse(sessionStorage.getItem("userId"))
+    }
+    else
+    {
+      this.userId=JSON.parse(localStorage.getItem("userId"))
+    }
+    this.startTime = Date.now();
+    this.startTime = Date.now();
+    this.createScreen()
+    if(JSON.parse(sessionStorage.getItem("bookmark160083"))==0)
+      this.bookmark=0
+    else if(this.bookmarkList.includes(this.screenNumber)||JSON.parse(sessionStorage.getItem("bookmark160083"))==1)
+      this.bookmark=1
+  }
+ 
+  createScreen()
+  {
+    this.service.createScreen({
+      "ScrId":0,
+      "ModuleId":this.moduleId,
+      "GSetID":this.screenType,
+      "ScreenNo":this.screenNumber
+    }).subscribe(res=>{})
+  }
+ 
+  receiveBookmark(e)
+  {
+    console.log(e)
+    if(e==true)
+      this.bookmark=1
+    else
+      this.bookmark=0
+    sessionStorage.setItem("bookmark160083",JSON.stringify(this.bookmark))
+  }
+ 
+  receiveAvDuration(e)
+  {
+    console.log(e)
+    this.avDuration=e
+  }
+ 
+  submitProgress()
+  {
+    this.endTime = Date.now();
+    this.totalTime = this.endTime - this.startTime;
+    this.router.navigate(['/teenagers/career-success/s160084'])
+    this.service.submitProgressAv({
+      "ScrNumber":this.screenNumber,
+      "UserId":this.userId,
+      "BookMark":this.bookmark,
+      "ModuleId":this.moduleId,
+      "screenType":this.screenType,
+      "timeSpent":this.totalTime,
+      "avDuration":this.avDuration
+    }).subscribe(res=>
+      { 
+        this.bookmarkList=res.GetBkMrkScr.map(a=>parseInt(a.ScrNo))
+        localStorage.setItem("bookmarkList",JSON.stringify(this.bookmarkList))
+      })
+  }
+
+  prev()
+  {
+    this.router.navigate(['/teenagers/career-success/s160082'])
+  }
+
+  ngOnDestroy()
+  {
+    localStorage.setItem("totalTime160083",this.totalTime)
+    localStorage.setItem("avDuration160083",this.avDuration)
+  }
+
+}
