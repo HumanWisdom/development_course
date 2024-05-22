61732b3df03f28ef3f0c94061bce95d3155e5a31
iamsanaasayyed <iamsanaasayyed@gmail.com>
2024-05-22 18:14:15 +0530

    ui changes
    c. add transcript content to the pathway-index & all 5 pathway screens

---
+++ HumanWisdom/projects/teenagers/src/app/teenagers/pathway/index-transcript/index-transcript.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index-transcript',
+  templateUrl: './index-transcript.page.html',
+  styleUrls: ['./index-transcript.page.scss'],
+})
+export class IndexTranscriptPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
