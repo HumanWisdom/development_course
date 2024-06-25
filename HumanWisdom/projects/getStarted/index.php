6a3bc7f637937a986325eedd03052a7a94829c30
betsyjohn0112 <betsyjohn0112@gmail.com>
2024-06-25 20:20:00 +0530

    minor changes
    minor changes

--- HumanWisdom/projects/getStarted/index.php
+++ HumanWisdom/projects/getStarted/index.php
@@ -10,6 +10,17 @@
     <!-- vendor_header -->
     <?php include('./includes/vendor_header.php'); ?>
     <!-- /vendor_header -->
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-44RHVTTNB8"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-44RHVTTNB8');
+    </script>
+    
   </head>
 
   <body>
