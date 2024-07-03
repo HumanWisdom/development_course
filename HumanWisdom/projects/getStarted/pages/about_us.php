aa6554a7d5f83bd68eb0516bbb18ecea1e84fca2
betsyjohn0112 <betsyjohn0112@gmail.com>
2024-07-03 19:49:15 +0530

    minor changes
    minor changes

--- HumanWisdom/projects/getStarted/pages/about_us.php
+++ HumanWisdom/projects/getStarted/pages/about_us.php
@@ -13,6 +13,14 @@
     <!-- vendor_header -->
     <?php include('../includes/vendor_header.php'); ?>
     <!-- /vendor_header -->
+
+       <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NHPHHF97');</script>
+      <!-- End Google Tag Manager -->
   </head>
 
   <body>
@@ -20,6 +28,11 @@
     <!-- header -->
     <?php include('../includes/header.php'); ?>
     <!-- /header -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NHPHHF97"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     <section class="hpt120px">
       <div class="row" data-aos="fade-up" data-aos-delay="100">
