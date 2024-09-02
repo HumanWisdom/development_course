fd4ccdd1fba4e6ec1ed2c619e4bffe929ef1fdf9
bharti <himanigupta027@gmail.com>
2024-09-02 08:08:22 +0530

    New teen web page to design from figma
    New teen web page to design

--- HumanWisdom/projects/getStarted/pages/education.php
+++ HumanWisdom/projects/getStarted/pages/education.php
@@ -138,6 +138,7 @@
                   </h4>
                 </div>
               </div>
+              
 
               <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 p0">
