982816bfa80f4e2642329cec48a3026e2f462ab7
PavishJain21 <jainpavish@gmail.com>
2024-09-12 17:51:59 +0530

    swipe up testing

--- HumanWisdom/projects/shared/component/s3-video/s3-video.component.ts
+++ HumanWisdom/projects/shared/component/s3-video/s3-video.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Location } from '@angular/common';
 import { NavigationService } from '../../services/navigation.service';
-
 @Component({
   selector: 'HumanWisdom-s3-video',
   templateUrl: './s3-video.component.html',
@@ -15,20 +14,20 @@ export class S3VideoComponent implements OnInit {
   public videoTitle: any;
   public linkcode: any;
   public wisdomshort: boolean = true;
-
+  @ViewChild('enablemodal') enablemodal: ElementRef;
   constructor(
     private route: ActivatedRoute,
     private _sanitizer: DomSanitizer,
     private location: Location,
     private router: Router,
-    private navigationService:NavigationService
+    private navigationService: NavigationService
   ) {
     let url: any = window.location.href;
     if (url.includes('videopage')) {
       this.wisdomshort = false;
       this.linkcode = this.route.snapshot.paramMap.get('videolink');
-      let name = this.linkcode.split('-videos')[0]
-      let link = this.linkcode.split('-videos')[1]
+      let name = this.linkcode.split('-videos')[0];
+      let link = this.linkcode.split('-videos')[1];
       this.linkcode = name + '/videos' + link.replaceAll('-', '/');
       if (this.linkcode.includes('teenagers')) {
         this.linkcode = this.linkcode.replaceAll('-', '/');
@@ -46,22 +45,29 @@ export class S3VideoComponent implements OnInit {
       code = `https://d1tenzemoxuh75.cloudfront.net/wisdom_shorts/videos/${this.linkcode}`;
     } else {
       code = `https://d1tenzemoxuh75.cloudfront.net/${this.linkcode}`;
-      // code = `https://d1tenzemoxuh75.cloudfront.net/${this.linkcode}`;
     }
     this.videoLink = this.getSafeUrl(code);
   }
 
   getSafeUrl(url) {
-    return this._sanitizer.bypassSecurityTrustResourceUrl(url)
+    return this._sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
+  onSwipeUp() {
+    console.log('Swiped Up!');
+    alert("Hiii in swipe up")
+    this.enablemodal.nativeElement.click();
+    // Implement logic for swipe up gesture
+    // Example: Navigate to the next video
+  }
 
   goBack() {
     var url = this.navigationService.navigateToBackLink();
     if (url == null) {
       this.location.back();
-    }else{
+    } else {
       this.router.navigate([url]);
     }
   }
+
 }
