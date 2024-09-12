847cd4e4013bef28a33b57902b1a2f11a19ff0b5
PavishJain21 <jainpavish@gmail.com>
2024-09-12 23:40:29 +0530

    fixed build issues

--- HumanWisdom/projects/shared/component/s3-video/s3-video.component.ts
+++ HumanWisdom/projects/shared/component/s3-video/s3-video.component.ts
@@ -84,7 +84,7 @@ export class S3VideoComponent implements OnInit {
       data = this.wisdomShortOrderList[++this.currentIndex]
     }
     this.videoTitle = data.shortsData.Title;
-    let linklist = this. data.shortsData.VideoUrl.split("/");
+    let linklist = data.shortsData.VideoUrl.split("/");
     this.linkcode = linklist[linklist.length-1];
     const  code = `https://d1tenzemoxuh75.cloudfront.net/wisdom_shorts/videos/${this.linkcode}`;
     this.videoLink = this.getSafeUrl(code);
