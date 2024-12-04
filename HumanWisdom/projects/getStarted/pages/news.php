8c7684c6dd21bf2959bd9c91c43e6aa1a7e194af
bharti <himanigupta027@gmail.com>
2024-12-04 12:54:06 +0530

    index
    index

---
+++ HumanWisdom/projects/getStarted/pages/news.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta property="og:title" id="tag1" content="HappierMe:For Teens & Adults" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="Master your mind and be happier" />
+    <meta property="og:image" content="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/website/imgs/website_share.jpg" />
+    <meta property="og:image:alt" content="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/website/imgs/website_share.jpg" />
+    <meta property="og:image:width" content="414" />
+    <meta property="og:image:height" content="232" />
+    <meta property="og:title" content="HappierMe:For Teens & Adults">
+    <meta property="og:site_name" content="HappierMe">
+    <meta property="og:url" content="https://happierme.app/">
+    
+    <!--Schema tag for Organization :-->
+    <script type="application/ld+json">
+  [
+    {
+      "@context": "https://schema.org",
+      "@type": "Article",
+      "name": "HappierMe",
+      "description": "Mental health app that helps users understand their emotions and thoughts, and offers a variety of tools and resources to help people improve their lives.",
+      "url": "https://happierme.app/"
+    },
+    {
+      "@context": "https://schema.org",
+      "@type": "MentalHealthService",
+      "name": "HappierMe",
+      "description": "Mental health app that helps users understand their emotions and thoughts, and offers a variety of tools and resources to help people improve their lives.",
+      "url": "https://happierme.app/"
+    }
+  ]
+</script>
+
+    <!-- vendor_header -->
+    <?php include('../includes/vendor_header.php'); ?>
+    <!-- /vendor_header -->
+  </head>
+
+  <body>
+
+    <!-- request a demo php script -->
+    <?php
+      // Simulate fetching country data from a database or external API
+      $countries = 
+     ["United States", "Canada", "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and/or Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Cook Islands", "Costa Rica", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecudaor", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France, Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kosovo", "Kuwait", "Kyrgyzstan", "Lao People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfork Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia South Sandwich Islands", "South Sudan", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbarn and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States minor outlying islands", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City State", "Venezuela", "Vietnam", "Virigan Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zaire", "Zambia", "Zimbabwe"]
+    ?>
+    <!-- /request a demo php script -->
+
+    <!-- <div class="row center_flex">
+      <div class="col-xl-4 col-lg-12 col-md-12 col-sm-12 col-xs-12 p0"> -->
+
+        <!-- header -->
+        <?php include('../includes/header.php'); ?>
+        <!-- /header -->
+
+        <section class="hpt120px">
+          <div class="row" data-aos="fade-up" data-aos-delay="100">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 p0">
+
+              <div class="row prelative" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 p0">
+                  <img src="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/website/webp/work_landing.webp" class="img-responsive w100p display_m_none" alt="Boost workplace wellbeing, performance and productivity">
+
+                  <img src="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/website/webp/work_mobile.webp" class="img-responsive w100p display_d_none" alt="Boost workplace wellbeing, performance and productivity">
+                </div>
+              </div>
+        
+              <div class="row center_flex absolute_desc absolute_desc_m" data-aos="fade-up" data-aos-delay="500">
+                <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12 p0 tleft">
+                  <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 p0 mt20px">
+                    <h1 class="mtb0px fs_36px fw_600 lh_140p fc_ffffff">
+                      Boost workplace wellbeing, performance and productivity
+                    </h1>
+                  </div>
+        
+                  <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 p0 mtb20px">
+                    <h5 class="mt20px mb30px fs_15px fw_400 lh_160p fc_ffffff">
+                      Upskill your staff to be happier, emotionally intelligent and make better decisions. An all-in-one app for mental health, relationships, better decision-making and soft skills.
+                    </h5>
+                  </div>
+        
+                  <!-- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 p0">
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 p0">
+                      <a href="#bring_happierme" class="">
+                        <button class="fs_15px fw_600 lh_140p fc_ffffff btn_tff" id="requestDemoForWork"  >
+                          Request a demo
+                        </button>
+                      </a>
+                    </div>
+                  </div> -->
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section>
+
+        <main id="main">
+
+          
+
+          
+
+          
+          
+
+          <!-- footer -->
+          <?php include('../includes/footer.php'); ?>
+          <!-- /footer -->
+
+        </main>
+
+      <!-- </div>
+    </div> -->
+
+    <!-- vendor_footer -->
+    <?php include('../includes/vendor_footer.php'); ?>
+    <!-- /vendor_footer -->
+
+  </body>
+
+</html>
