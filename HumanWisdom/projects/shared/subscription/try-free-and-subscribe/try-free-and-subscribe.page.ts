da08a2fcdf7ff601d42fc5639d5d19ebde46e16b
PavishJain21 <jainpavish@gmail.com>
2025-01-10 15:23:38 +0530

    done changes for payment popup survey

--- HumanWisdom/projects/shared/subscription/try-free-and-subscribe/try-free-and-subscribe.page.ts
+++ HumanWisdom/projects/shared/subscription/try-free-and-subscribe/try-free-and-subscribe.page.ts
@@ -18,7 +18,7 @@ import { CommonService } from '../../services/common.service';
 export class TryFreeAndSubscribePage implements OnInit {
 
   selectedSubscription: string;
-  countryCode: string='USA';
+  countryCode: string = 'USA';
   defaultCountry: string;
   defaultCurrencySymbol: string;
   Monthly: string;
@@ -27,28 +27,28 @@ export class TryFreeAndSubscribePage implements OnInit {
   paymentIntentModel: paymentIntentModel;
   pricingModel: any;
   userId: number;
-  trialStatus : string = 'No Trial';
+  trialStatus: string = 'No Trial';
   isFromCancelled: boolean = false;
   cartList = [];
-  startDate:any;
-  expDate:any;
+  startDate: any;
+  expDate: any;
   isAdults = true;
   constructor(private router: Router, private onboardingService: OnboardingService,
     public logeventservice: LogEventService,
-    private navigateService:NavigationService,
+    private navigateService: NavigationService,
     private location: Location,
-  private commonService:CommonService) {
+    private commonService: CommonService) {
     this.Monthly = Constant.MonthlyPlan;
     this.Annual = Constant.AnnualPlan;
     this.Redeem = Constant.Redeem;
-    this.onboardingService.checkTrial().subscribe(res=>{
-      if(res){
+    this.onboardingService.checkTrial().subscribe(res => {
+      if (res) {
         this.trialStatus = res[0].IsTrial;
-         if(this.trialStatus != Constant.NoTrial){
+        if (this.trialStatus != Constant.NoTrial) {
           this.startDate = res[0].StartDate;
           this.expDate = res[0].ExpDate;
-         }
-         SharedService.setDataInLocalStorage('trialStatus',this.trialStatus);
+        }
+        SharedService.setDataInLocalStorage('trialStatus', this.trialStatus);
       }
     })
     if (SharedService.ProgramId == ProgramType.Adults) {
@@ -66,21 +66,21 @@ export class TryFreeAndSubscribePage implements OnInit {
   }
 
   getUserDetails() {
-      this.onboardingService.getuser(this.userId).subscribe(res => {
-        if (res) {
-          let userDetails = res[0];
-          localStorage.setItem('userDetails',res[0]);
-          localStorage.setItem("isPartner", userDetails.IsPartner);
-          localStorage.setItem("PartnerOption", userDetails.PartnerOption);
-          localStorage.setItem("SubscriberType", res[0].SubscriberType);
-          this.onboardingService.getUserDetails.next(true);
-        }
-      });
+    this.onboardingService.getuser(this.userId).subscribe(res => {
+      if (res) {
+        let userDetails = res[0];
+        localStorage.setItem('userDetails', res[0]);
+        localStorage.setItem("isPartner", userDetails.IsPartner);
+        localStorage.setItem("PartnerOption", userDetails.PartnerOption);
+        localStorage.setItem("SubscriberType", res[0].SubscriberType);
+        this.onboardingService.getUserDetails.next(true);
+      }
+    });
   }
 
   InitializeDefaultValues() {
     let canclled = SharedService.getDataFromLocalStorage(Constant.isFromCancelled);
-    this.isFromCancelled =  (canclled == null || canclled == undefined || canclled =='null' || canclled =='') ? false : (canclled==Constant.ShortTrue) ? true:false;
+    this.isFromCancelled = (canclled == null || canclled == undefined || canclled == 'null' || canclled == '') ? false : (canclled == Constant.ShortTrue) ? true : false;
     var sub = SharedService.getDataFromLocalStorage(Constant.HwpSubscriptionPlan);
     if (sub == Constant.AnnualPlan ||
       sub == Constant.MonthlyPlan) {
@@ -115,11 +115,11 @@ export class TryFreeAndSubscribePage implements OnInit {
   }
 
   SelectSubscriptionType(subscriptionType: string) {
-    this.logeventservice.logEvent("click_select_"+subscriptionType);
+    this.logeventservice.logEvent("click_select_" + subscriptionType);
 
     if (subscriptionType != Constant.Redeem) {
       SharedService.setDataInLocalStorage(Constant.HwpSubscriptionPlan, subscriptionType);
-    }else if(subscriptionType == Constant.Redeem) {
+    } else if (subscriptionType == Constant.Redeem) {
       this.router.navigate([`/${SharedService.getprogramName()}/redeem-subscription`])
     }
     this.selectedSubscription = subscriptionType;
@@ -127,25 +127,23 @@ export class TryFreeAndSubscribePage implements OnInit {
   }
 
   tryFreeSubscribe() {
-    if(this.trialStatus =="No Trial")
-    {
+    if (this.trialStatus == "No Trial") {
 
       this.logeventservice.logEvent("click_try_free_subscribe");
     }
-    else if(this.trialStatus =="ended")
-    {
+    else if (this.trialStatus == "ended") {
       this.logeventservice.logEvent("click_proceed_subscribe");
-      
+
     }
 
-  
+
     if (this.CheckIfUserIsLoggedIn()) {
       this.SetPaymentIntentModel();
       this.SetDataInLocalStorage();
       if (this.selectedSubscription != this.Redeem) {
-        if(this.trialStatus == 'No Trial'){
+        if (this.trialStatus == 'No Trial') {
           this.router.navigateByUrl(`/${SharedService.getprogramName()}/subscription/proceed-to-payment`);
-        }else {
+        } else {
           this.router.navigateByUrl(`/${SharedService.getprogramName()}/subscription/proceed-to-payment`);
           // SharedService.setDataInLocalStorage(Constant.isFromCancelled,'');
           // var amt = this.selectedSubscription == Constant.AnnualPlan ? this.pricingModel.Annual : this.pricingModel.Monthly;
@@ -164,18 +162,18 @@ export class TryFreeAndSubscribePage implements OnInit {
   }
 
 
-  dashboard(){
+  dashboard() {
     this.router.navigateByUrl(SharedService.getDashboardUrls());
   }
 
-  checkout(){
-    SharedService.setDataInLocalStorage(Constant.isFromCancelled,'');
+  checkout() {
+    SharedService.setDataInLocalStorage(Constant.isFromCancelled, '');
     var amt = this.selectedSubscription == Constant.AnnualPlan ? this.pricingModel.Annual : this.pricingModel.Monthly;
-    localStorage.setItem('totalAmount',amt);
-    SharedService.setDataInLocalStorage(Constant.Checkout,'T')
-    this.router.navigate([`/${SharedService.getprogramName()}/onboarding/payment`], { state: { quan: this.cartList.length.toString(), plan: this.selectedSubscription, rateId:this.pricingModel.RateID }})
+    localStorage.setItem('totalAmount', amt);
+    SharedService.setDataInLocalStorage(Constant.Checkout, 'T')
+    this.router.navigate([`/${SharedService.getprogramName()}/onboarding/payment`], { state: { quan: this.cartList.length.toString(), plan: this.selectedSubscription, rateId: this.pricingModel.RateID } })
   }
- getCountry() {
+  getCountry() {
     this.onboardingService.getCountry().subscribe((res: any) => {
       if (res[Constant.In_eu]) {
         this.countryCode = Constant.EUR;
@@ -218,14 +216,14 @@ export class TryFreeAndSubscribePage implements OnInit {
 
 
   SetPaymentIntentModel() {
-    const affref =localStorage.getItem('AffReferralCode');
+    const affref = localStorage.getItem('AffReferralCode');
     this.paymentIntentModel = {
       DiscountCode: "0",
       PlanID: this.selectedSubscription == Constant.MonthlyPlan ? SubscriptionType.Monthly.toString() : SubscriptionType.Annual.toString(),
       ProgID: SharedService.ProgramId.toString(),
       RateID: this.pricingModel?.RateID?.toString(),
       UserID: this.userId.toString(),
-      AffReferralCode: (affref == null || affref == undefined) ?  '' : affref
+      AffReferralCode: (affref == null || affref == undefined) ? '' : affref
     } as paymentIntentModel
   }
 
@@ -237,25 +235,31 @@ export class TryFreeAndSubscribePage implements OnInit {
   }
 
   back() {
-    this.commonService.updateSurveyData(2);
-    var url = this.navigateService.goBack();
-    this.router.navigateByUrl(url);
+    if (sessionStorage.getItem('isPaymentBackClicked') && sessionStorage.getItem('isPaymentBackClicked') == 'T') {
+      var url = this.navigateService.goBack();
+      this.router.navigateByUrl(url);
+    } else {
+      sessionStorage.setItem('isPaymentBackClicked', 'T');
+      this.commonService.updateSurveyData(2);
+      var url = this.navigateService.goBack();
+      this.router.navigateByUrl(url);
     }
-    
+  }
+
   routeToDashboard() {
     this.router.navigateByUrl(SharedService.getDashboardUrls());
   }
 
-  buyGift(){
+  buyGift() {
     this.router.navigateByUrl(`/${SharedService.getprogramName()}/give-the-gift-of-wisdom`);
   }
 
-  terms(){
+  terms() {
     this.router.navigateByUrl('/terms-and-conditions');
   }
 
-  getIsoCode(){
-    if(this.pricingModel.CurSymbol == '$'){
+  getIsoCode() {
+    if (this.pricingModel.CurSymbol == '$') {
       return ` (${this.pricingModel.ISOCode})`;
     }
     return '';
