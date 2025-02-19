2b42bd2018dbc8a0cfb9134d61a08a237346068a
pavishjain21 <jainpavish@gmail.com>
2025-02-19 13:04:58 +0530

    done changes

--- HumanWisdom/projects/shared/component/repeat-user/my-daily-practice/my-daily-practice.page.ts
+++ HumanWisdom/projects/shared/component/repeat-user/my-daily-practice/my-daily-practice.page.ts
@@ -71,7 +71,10 @@ export class MyDailyPracticePage implements OnInit {
 
         this.dailyInsModule = res.split(';')[2] ? res.split(';')[2]?.toString()?.replaceAll('/', '') : "";
        // this.DailyInspirationImg = "https://humanwisdoms3.s3.eu-west-2.amazonaws.com/daily_inspiration/portrait" + this.DailyInspirationLink.substring(this.DailyInspirationLink.lastIndexOf('/')).toString().replace("mp4", "webp")
-        this.DailyInspirationImg = "https://humanwisdoms3.s3.eu-west-2.amazonaws.com/daily_inspiration/portrait_new/" + this.DailyInspirationLink.substring(this.DailyInspirationLink.lastIndexOf('/')).toString().split('.')[1].toString()  +".webp"
+       
+       //https://d1tenzemoxuh75.cloudfront.net/assets/images/tiles/dashboard/wisdom_shorts/wisdom_shorts_109.webp 
+       
+       this.DailyInspirationImg = "https://d1tenzemoxuh75.cloudfront.net/assets/images/tiles/dashboard/wisdom_shorts/wisdom_shorts_" + this.DailyInspirationLink.substring(this.DailyInspirationLink.lastIndexOf('/')).toString().split('.')[1].toString()  +".webp"
 
        this.enableVideo = true;
       }
