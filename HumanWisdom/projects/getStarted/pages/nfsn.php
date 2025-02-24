fac3ee67ab6fc898048555ef4c0189da339b6815
bharti <himanigupta027@gmail.com>
2025-02-24 15:59:04 +0530

    podcast
    podcast

--- HumanWisdom/projects/getStarted/pages/nfsn.php
+++ HumanWisdom/projects/getStarted/pages/nfsn.php
@@ -51,7 +51,7 @@
 
           <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12 col-xs-12 p0"  data-aos="fade-up" data-aos-delay="100">
             
-              <img style="height:550px;width:380px"
+              <img style="height:500px;width:327px;"
                 src="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/website/nfsn.webp"class="img-responsive w100p display_m_none"
                 >
                 <img src="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/website/webp/nfsn_new.webp" class="img-responsive w100p display_d_none"  loading="lazy"alt="performance">
@@ -61,17 +61,17 @@
           <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12 col-xs-12 p0 cml40px"  data-aos="fade-up" data-aos-delay="200">
             <div class="row">
               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 p0">
-              <div class="row mt20px">
+              <div class="row">
               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 p0 ddflex_mblock_a">
-               <!-- <img src="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/website/webp/frame.webp" class="img-responsive img_aspects"  alt="wh_tick_black">  -->
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6 p0 mb15px mr15px">
+               <img src="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/website/svgs/nfsnboth.svg" class="img-responsive img_aspects"  alt="wh_tick_black"> 
+                <!-- <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6 p0 mb15px mr15px">
                     
                 <img src="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/website/svgs/happierme1.svg"  class="img-responsive w100p display_m_none" alt="wh_tick_black" style="width:177px;height:50px;">
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6 p0 mb15px mr15px plr20px"> 
                 <img src="https://humanwisdoms3.s3.eu-west-2.amazonaws.com/website/svgs/trans1.svg"  class="img-responsive w100p display_m_none"  alt="wh_tick_black"style="width:225px;height:60px;"> 
                 
-            </div> 
+            </div>  -->
               </div>
             </div>
 
