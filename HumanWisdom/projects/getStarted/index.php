aa44aafe0eb58aa7af1a8242ff1697ecff59fce9
betsyjohn0112 <97895908+betsyjohn0112@users.noreply.github.com>
2025-02-26 13:18:19 +0530

    Update index.php
    tagline changed again

--- HumanWisdom/projects/getStarted/index.php
+++ HumanWisdom/projects/getStarted/index.php
@@ -129,7 +129,7 @@
             <div class="row mtb15px">
               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 p0">
                 <h2 class="mtb0px fs_24px fw_400 lh_140p fc_000000 ta_lc">
-                Have fulfilling relationships, manage your emotions and succeed in life. </h2>
+                Manage your emotions, have happier relationships, and succeed in life. </h2>
               </div>
             </div>
 
