6ba541fc74459e85ef400a424338c3ab5f9dc169
pavishjain21 <jainpavish@gmail.com>
2025-03-19 00:14:28 +0530

    done changes

--- HumanWisdom/projects/getStarted/gulpfile.js
+++ HumanWisdom/projects/getStarted/gulpfile.js
@@ -1,23 +1,32 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
 
-// Define the CSS minify task for each folder
-function minifyCSS() {
-    return gulp.src('assets-min/css/*.css')  // Process CSS in 'assets-min/css/'
-        .pipe(cleanCSS({ compatibility: 'ie8' }))  // Minify CSS
-        .pipe(gulp.dest('assets-min/css/'))  // Output to 'assets-min/css/'
+function minifyCSSAndJS() {
+    return gulp.src('assets-min/css/*.css')  
+        .pipe(cleanCSS({ compatibility: 'ie8' }))  
+        .pipe(gulp.dest('assets-min/css/'))  
 
-        .pipe(gulp.src('assets-min/font/*.css'))  // Process CSS in 'assets-min/font/'
-        .pipe(cleanCSS({ compatibility: 'ie8' }))  // Minify CSS
-        .pipe(gulp.dest('assets-min/font/'))  // Output to 'assets-min/font/'
+        .pipe(gulp.src('assets-min/font/*.css'))  
+        .pipe(cleanCSS({ compatibility: 'ie8' }))  
+        .pipe(gulp.dest('assets-min/font/'))  
 
-        .pipe(gulp.src('assets/css/*.css'))  // Process CSS in 'assets-min/font/'
-        .pipe(cleanCSS({ compatibility: 'ie8' }))  // Minify CSS
-        .pipe(gulp.dest('assets/css/'));  // Output to 'assets-min/font/'
+        .pipe(gulp.src('assets/css/*.css'))  
+        .pipe(cleanCSS({ compatibility: 'ie8' })) 
+        .pipe(gulp.dest('assets/css/'))
+        
+        .pipe(gulp.src('scripts/*.js')) 
+       .pipe(uglify()) 
+       .pipe(gulp.dest('scripts/'))
+       
+       .pipe(gulp.src('assets/js/*.js')) 
+       .pipe(uglify()) 
+       .pipe(gulp.dest('assets/js/'))
+
 }
 
 // Use gulp.series to ensure task completion
-gulp.task('minify-css', minifyCSS);
+gulp.task('minify-css-js', minifyCSSAndJS);
 
 // Default task (run when you execute `gulp` without any task name)
-gulp.task('default', gulp.series('minify-css'));
+gulp.task('default', gulp.series('minify-css-js'));
