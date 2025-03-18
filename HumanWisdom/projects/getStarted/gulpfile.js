10320601e01d1af570397410f9eef8491d572435
pavishjain21 <jainpavish@gmail.com>
2025-03-19 00:28:10 +0530

    done changes

--- HumanWisdom/projects/getStarted/gulpfile.js
+++ HumanWisdom/projects/getStarted/gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 
-function minifyCSSAndJS() {
+function minifyCss() {
     return gulp.src('assets-min/css/*.css')  
         .pipe(cleanCSS({ compatibility: 'ie8' }))  
         .pipe(gulp.dest('assets-min/css/'))  
@@ -14,19 +14,21 @@ function minifyCSSAndJS() {
         .pipe(gulp.src('assets/css/*.css'))  
         .pipe(cleanCSS({ compatibility: 'ie8' })) 
         .pipe(gulp.dest('assets/css/'))
-        
-        .pipe(gulp.src('scripts/*.js')) 
+}
+
+function minifyJS() {
+    return gulp.src('scripts/*.js')
        .pipe(uglify()) 
        .pipe(gulp.dest('scripts/'))
        
        .pipe(gulp.src('assets/js/*.js')) 
        .pipe(uglify()) 
        .pipe(gulp.dest('assets/js/'))
-
 }
 
-// Use gulp.series to ensure task completion
-gulp.task('minify-css-js', minifyCSSAndJS);
 
-// Default task (run when you execute `gulp` without any task name)
-gulp.task('default', gulp.series('minify-css-js'));
+// Use gulp.series to ensure task completion
+gulp.task('minify-css', minifyCss);
+gulp.task('minify-js', minifyJS);
+
+gulp.task('default', gulp.series('minify-css','minify-js'));
