6854c02ce365f8db9f94e1d46636df6eda2476f7
pavishjain21 <jainpavish@gmail.com>
2025-04-14 13:45:06 +0530

    done changes

--- HumanWisdom/projects/shared/component/find-inspiration/find-inspiration.component.ts
+++ HumanWisdom/projects/shared/component/find-inspiration/find-inspiration.component.ts
@@ -12,7 +12,7 @@ export class FindInspiration  {
   inspirationItems = [
     { 
       title: 'Podcasts', 
-      img: 'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/svgs/v1_3/fi_audio_meditation.svg',
+      img: 'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/icons/podcast_without_bg.png',
       icon: 'headphones', 
       height:'',
       color: '#191d3a',
@@ -36,12 +36,12 @@ export class FindInspiration  {
       url:'wisdom-shorts'
     },  
     { 
-      title: 'Life Stories', 
-      icon: 'volume-2', 
+      title: 'PATHWAY', 
+      icon: 'star', 
       color: '#191d3a',
       height:'',
-      img: 'https://d1tenzemoxuh75.cloudfront.net/assets/svgs/v1_3/fi_life_stories.svg',
-      url:'wisdom-stories'
+      img:"https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/svgs/v1_3/dots.svg",
+      url:'pathway'
     },
     { 
       title: 'Audio meditation', 
@@ -51,6 +51,23 @@ export class FindInspiration  {
       img: 'https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/svgs/v1_3/fi_audio_meditation.svg',
       url:'audio-meditation'
     },
+    { 
+      title: 'Contact a coach', 
+      icon: 'message-circle', 
+      color: '#191d3a',
+      height:'h-24',
+      img:"https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/svgs/v1_3/contact_coach_teens.svg",
+      url:'coach'
+    },
+    { 
+      title: 'Life Stories', 
+      icon: 'volume-2', 
+      color: '#191d3a',
+      height:'',
+      img: 'https://d1tenzemoxuh75.cloudfront.net/assets/svgs/v1_3/fi_life_stories.svg',
+      url:'wisdom-stories'
+    },
+    
     { 
       title: 'Blog', 
       icon: 'volume-2', 
@@ -66,24 +83,9 @@ export class FindInspiration  {
       height:'',
       img: 'https://d1tenzemoxuh75.cloudfront.net/assets/svgs/v1_3/fi_events.svg',
       url:'events'
-    },
+    },   
     
-    { 
-      title: 'PATHWAY', 
-      icon: 'star', 
-      color: '#191d3a',
-      height:'',
-      img:"https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/svgs/v1_3/dots.svg",
-      url:'pathway'
-    },
-    { 
-      title: 'Contact a coach', 
-      icon: 'message-circle', 
-      color: '#191d3a',
-      height:'h-24',
-      img:"https://humanwisdoms3.s3.eu-west-2.amazonaws.com/assets/svgs/v1_3/contact_coach_teens.svg",
-      url:'coach'
-    }
+    
   ];
 
 
