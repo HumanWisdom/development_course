c91c497d0c5c393b86362965126a97c34664aed9
bharti <himanigupta027@gmail.com>
2025-04-28 10:12:03 +0530

    blogs
    blogs

--- HumanWisdom/projects/getStarted/blogs/alarming_impact_of_work_stress.php
+++ HumanWisdom/projects/getStarted/blogs/alarming_impact_of_work_stress.php
@@ -178,117 +178,73 @@
 
 
                 <h4 class="mtb0px blog_desc">
-                Common issues caused by stress-induced sleep problems include:
+                Stress-reduction techniques:
+
+:
+               
                   <ul>
                     <li>
-                    Breathing exercises to activate the body’s relaxation response
-                    </li>
+                    Breathing exercises to activate the body’s relaxation response                    </li>
 
                     <li>
-                    Guided meditations to calm an anxious mind before bedtime </li>
+                    Guided meditations to calm an anxious mind before bedtime                    </li>
 
                     <li>
                     Journaling prompts to release worries and promote mental clarity                    </li>
 
-                    <li>
-                    Tapping exercises to calm the mind
-                    </li>
+                   <li>
+                   Tapping exercises to calm the mind
+                </li>
                   </ul>
                 </h4>
+                
+
+
                 <h4 class="mtb0px blog_desc">
-                Managing stress effectively is key to improving sleep quality. The HappierMe app offers science-backed techniques to help users break the stress-sleep cycle and regain control of their well-being.                </h4>
+                Long-term solutions for better sleep
+
+:
+               
+                  <ul>
+                    <li>
+                    Building self-awareness: Recognizing stress triggers and reframing responses
+                    </li>
+
+                    <li>
+                    Emotional intelligence training: Learning how to manage stress more effectively
+                    </li>
+
+                    <li>
+                    A regular meditation practice: This can help make the mind less reactive and better able to cope with the challenges of work
+                    </li>
+
+                   
+                  </ul>
+                </h4>
+                
 
                 <h4 class="mtb0px blog_title">
-                Stress-reduction techniques:                  
-                <span class="blog_highlight_black">
-                  until we arrive at the top of our imagined mountain, as president of this, or head of that, or rich and famous, and discover our hearts are still empty, 
-                  </span>
-                  and wonder where we went wrong. To numb the pain we feel we may turn to drink or drugs, and a downward spiral begins. So many rich and famous people have walked that well trodden path.
+                The HappierMe Approach              
+                
+                <h4 class="mtb0px blog_desc">
+                At HappierMe, we believe that sleep and mental well-being are deeply connected.
                 </h4>
 
                 <h4 class="mtb0px blog_desc">
-                  In the pursuit of success we may not have looked after our health or paid enough attention to our relationships and may find both in disarray, further contributing to our unhappiness. In any pursuit there is always the chance of disappointment and failure, and that can make us stressed, anxious, or depressed. Or we may 
-                  <a class="blog_highlight_peach td_underline" href="https://happierme.app/adults/comparison">
-                    compare ourselves with others, 
-                  </a>
-                  and feel less than them and think our lives are a failure, when in fact we may have much to be grateful for. It is a paradox that the journey to success which we thought would make us happier, may in fact end up doing the opposite.
+                We cannot always control our work environment, but we can take charge of how we react to those challenges, and this can create lasting change.
                 </h4>
 
                 <h4 class="mtb0px blog_desc">
-                  To understand what is going on in our mind we need to be curious and understand the many hidden drivers in our own thinking that create this framework in which we live our lives. Understanding these drivers can help us take charge of our own lives, and live a more balanced, happier life.
+                 
+                The HappierMe app has many resources to help including a detailed mental health section, a feel better now section, and mini workouts for developing a healthy mind.
                 </h4>
 
                 <h4 class="mtb0px blog_desc">
-                  We are 
-                  <span class="blog_highlight_black">
-                    all influenced (conditioned) by our environment
-                  </span>
-                  and not aware of it. If everyone is running in one direction we start running after them, without pausing to ask why. If people around us are obese we are much more likely to be obese, for example, or if everyone is busy chasing money, we are likely to do the same. Our conditioning influences come from our parents, our friends, and the media we consume.
+                 
+                Sleep shouldn’t be a casualty of work-related stress. With the right tools and mindset, professionals can regain control, sleep better, and wake up ready to face the day with clarity and confidence.
                 </h4>
 
-                <h4 class="mtb0px blog_desc">
-                  <span class="blog_highlight_black">
-                    Inside all of us is an inner emptiness that we may not be aware of.  
-                  </span>
-                  It makes us feel uncomfortable and pushes us to escape from it. It is why we are restless, always chasing something and never satisfied. When we are chasing something we feel a dopamine rush that disappears when we get what we want. We then feel empty again and need to chase something else.  This is why our minds are wired to assume that the more we accumulate on the outside, the better we will feel on the inside. This conditioning is powerful and we react when anyone suggests it may not be true.
-                </h4>
-
-                <h4 class="mtb0px blog_desc">
-                  I once met a lady who had 176 pairs of shoes, and was about to go shopping for the next pair. She did not understand the emptiness within her that was pushing her to do this, nor the futility of it. When challenged she could of course justify her actions. Our minds are very capable of justifying everything we do. There is nothing wrong with buying shoes, except the happiness we seek does not last long.
-                </h4>
-
-                <h4 class="mtb0px blog_desc">
-                  To experience 
-                  <span class="blog_highlight_black">
-                    true happiness which is a mix of contentment and joy,
-                  </span>
-                  we also need to begin an inner journey to find out what real happiness is, and find that peace for ourselves. How do we begin?
-                </h4>
-
-                <h4 class="mtb0px blog_desc">
-                  The first step is to realise that 
-                  <span class="blog_highlight_black">
-                    the happiness we seek in the world outside is already there inside us
-                  </span>
-                  and to find it we need to go on a journey of learning about ourselves and how our minds work. Be curious. Ask questions of yourself. Why is my mind so restless, always chasing the next thing? Why is the happiness I seek in the world so short-lived? Why I am blind to the extraordinary beauty of the world? Is my pursuit of success actually making me more unhappy?
-                </h4>
-
-                <h4 class="mtb0px blog_desc">
-                  <span class="blog_highlight_black">
-                    If you do want to begin your journey to discover this sense of peace and happiness within you, 
-                  </span>
-                  the 
-                  <a class="blog_highlight_peach td_underline" href="https://happierme.app/">
-                    HappierMe app 
-                  </a>
-                  (formerly HumanWisdom) has all the 
-                  <a class="blog_highlight_peach td_underline" href="https://happierme.app/adults/awareness">
-                    self-awareness tools 
-                  </a>
-                  to help you do so. The additional benefit of this deeper self-understanding is that it helps you understand others better, which leads to 
-                  <a class="blog_highlight_peach td_underline" href="https://happierme.app/adults/relationships">
-                    happier relationships. 
-                  </a>
-                  It also makes you emotionally intelligent, 
-                  <a class="blog_highlight_peach td_underline" href="https://happierme.app/adults/communication">
-                    a better communicator 
-                  </a>
-                  and more resilient, and this contributes to your success in the world.
-                </h4>
-
-                <h4 class="mtb0px blog_desc">
-                  If you are happy, content with who you are and what you have, and have a peaceful relationship with yourself and others, you can count yourself as successful already. If not, you can get started on that journey.
-                </h4>
-
-                <h4 class="mtb0px blog_desc">
-                  In summary, this deeper self-understanding can help you feel happier on the inside, and contribute to your success in the world.
-                </h4>
-
-                <h4 class="mtb0px blog_desc">
-                  Take the first step, and begin. Your happiness (and success) depends on it.
-                </h4>
-              </div>
-            </div>
+               
 
             <div class="row">
               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 p0">
