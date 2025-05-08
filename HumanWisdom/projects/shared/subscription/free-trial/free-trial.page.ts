aa4eaea0cbe1b38ed7a6eb64bd5451a8a5d3515b
pthisaiveerapandian <pthisaiveerapandian@gmail.com>
2025-05-08 14:46:15 +0530

    code changes

--- HumanWisdom/projects/shared/subscription/free-trial/free-trial.page.ts
+++ HumanWisdom/projects/shared/subscription/free-trial/free-trial.page.ts
@@ -35,9 +35,14 @@ export class FreeTrialPage implements OnInit {
     let addtraction = localStorage.getItem('callAddtraction');
 
     if(addtraction === 'Y') {
-      setTimeout(() => {
+      let userId = JSON.parse(localStorage.getItem("userId"))
+      this.onboardingService.getOrderId(userId).subscribe(res => {
+        localStorage.setItem('stripeid', res);
         this.payementSubmitBtnClick.nativeElement.click();
-      }, 2000);
+      }, (err) => {
+        
+      }
+      )
     }
     
     this.GetDataFromLocalStorage();
