aae4fa8608b45324c9234efa5c28f25c434c9738
betsyjohn0112 <betsyjohn0112@gmail.com>
2025-05-30 11:08:45 +0530

    timeout increased for order capture in adtraction

--- HumanWisdom/projects/shared/subscription/payment/payment.page.ts
+++ HumanWisdom/projects/shared/subscription/payment/payment.page.ts
@@ -198,7 +198,7 @@ export class PaymentPage implements OnInit, AfterViewInit {
           this.router.navigateByUrl(`/${SharedService.getprogramName()}/subscription/free-trial`);
         }
       });
-    }, 5000)
+    }, 9000)
   }
 
   back() {
