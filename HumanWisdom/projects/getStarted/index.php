781c398cad2cdaad0b26e8a98476b40ca1c9a1d4
betsyjohn0112 <betsyjohn0112@gmail.com>
2025-08-08 15:51:07 +0530

    minor changes

--- HumanWisdom/projects/getStarted/index.php
+++ HumanWisdom/projects/getStarted/index.php
@@ -3662,7 +3662,7 @@ The HappierMe app for teens has been designed to help them manage their emotions
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 p0 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 tcenter">
-                  <img src="https://d1tenzemoxuh75.cloudfront.net/website/svgs/parents.svg" class="img-responsive" alt="Mental Health" loading="lazy">
+                  <img src="https://d1tenzemoxuh75.cloudfront.net/website/svgs/build_d.svg" class="img-responsive" alt="Mental Health" loading="lazy">
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
                 </div>
@@ -4521,7 +4521,7 @@ The HappierMe app for teens has been designed to help them manage their emotions
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 p0" data-aos="fade-up" data-aos-delay="200">
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 tcenter">
-                    <img src="https://d1tenzemoxuh75.cloudfront.net/website/svgs/teebagers.svg" class="img-responsive" alt="Mental Health" loading=lazy>
+                    <img src="https://d1tenzemoxuh75.cloudfront.net/website/svgs/parents.svg" class="img-responsive" alt="Mental Health" loading=lazy>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 p0" data-aos="fade-up" data-aos-delay="200">
                 </div>
