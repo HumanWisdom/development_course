0da13ec9a0c15d9cd7b4e428f89435aeeb962c5c
betsyjohn0112 <betsyjohn0112@gmail.com>
2025-10-13 20:31:36 +0530

    New Website Blog added

--- HumanWisdom/projects/getStarted/blogs/blog_index.php
+++ HumanWisdom/projects/getStarted/blogs/blog_index.php
@@ -1170,6 +1170,41 @@
               </a>
             </div>
 
+            
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 col-12 rp0 aos-init aos-animate" data-aos="fade-up" data-aos-delay="1100">
+
+              <a class="" href="10-ways-to-thrive-as-a-parent.php">
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12 p0">
+                    <img src="https://d1tenzemoxuh75.cloudfront.net/blogs/723.webp" class="img-responsive img_blogs" alt="Why Do Leaders Fail? Insights from a new poll">
+                  </div>
+                </div>
+
+                <div class="row mt20px">
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12 p0">
+                    <button class="mtb0px fs_12px fw_400 lh_150p fc_834b66 btn_blogp">Parenting</button>
+
+
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12">
+                    <h4 class="mt20px mb10px fs_18px fw_500 lh_140p fc_000000">10 Ways to Thrive as a Parent</h4>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12 p0">
+                    <h6 class="mtb12px fs_12px fw_400 lh_150p fc_000000_0_5">
+
+
+
+                    </h6>
+                  </div>
+                </div>
+              </a>
+            </div>
+
           </span>
             <div class="clearfix"></div>
 
