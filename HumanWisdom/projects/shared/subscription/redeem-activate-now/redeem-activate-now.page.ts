f13c804c992c157e89d72e6ece10042114e2d831
betsyjohn0112 <betsyjohn0112@gmail.com>
2025-11-03 14:06:47 +0530

    scss deleted for payment and redeem-active-now

--- HumanWisdom/projects/shared/subscription/redeem-activate-now/redeem-activate-now.page.ts
+++ HumanWisdom/projects/shared/subscription/redeem-activate-now/redeem-activate-now.page.ts
@@ -6,7 +6,6 @@ import { SharedService } from '../../services/shared.service';
 @Component({
   selector: 'app-redeem-activate-now',
   templateUrl: './redeem-activate-now.page.html',
-  styleUrls: ['./redeem-activate-now.page.scss'],
 })
 export class RedeemActivateNowPage implements OnInit {
 
