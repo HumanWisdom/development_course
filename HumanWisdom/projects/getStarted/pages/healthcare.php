6a373356c0912cdfbcd9fcad66712fc61e58d692
pavishjain21 <jainpavish@gmail.com>
2025-11-26 15:41:07 +0530

    commit for changes

--- HumanWisdom/projects/getStarted/pages/healthcare.php
+++ HumanWisdom/projects/getStarted/pages/healthcare.php
@@ -1134,18 +1134,17 @@
                 </a>-->
               </div>
 
-              <div class="owl-theme">
+              <div class="owl-theme mt40px">
                 <div class="owl-controls">
-                  <div class="owl-nav">
-
-                  <a class="sap" href="https://happierme.app/adults/testimonials">
-                      <h4 class="mtb0px fs_18px fw_500 lh_150p fc_cb6171 td_underline">
-                      View all Success stories
-                      </h4>
-                    </a>
-                  </div>
+                  <div class="owl-nav owl-nav-w"></div>
                 </div>
               </div>
+
+              <a class="sap" href="https://happierme.app/adults/testimonials">
+                <h4 class="mtb0px fs_18px fw_500 lh_150p fc_cb6171 td_underline">
+                  View all Success stories
+                </h4>
+              </a>
             </div>
 
           </div>
