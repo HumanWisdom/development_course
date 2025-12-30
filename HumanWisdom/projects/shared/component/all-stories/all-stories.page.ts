f04183e3f03d1cddb02be63dfbbf2003e162e3fc
bharti <himanigupta027@gmail.com>
2025-12-30 11:19:03 +0530

    life stories find answer questions

--- HumanWisdom/projects/shared/component/all-stories/all-stories.page.ts
+++ HumanWisdom/projects/shared/component/all-stories/all-stories.page.ts
@@ -165,15 +165,15 @@ export class AllStoriesPage implements OnInit {
 
   toggle_view_more_less()
   {
-    if(this.view_more_less == "View More")
+    if(this.view_more_less == "View more")
     {
       this.enable_view_more_less = true;
-      this.view_more_less = "View Less";
+      this.view_more_less = "View less";
     }
     else
     {
       this.enable_view_more_less = false;
-      this.view_more_less = "View More";
+      this.view_more_less = "View more";
     }
   }
 
