2178bbe8ad30db551a158262119ead25cd690119
bharti <himanigupta027@gmail.com>
2026-03-18 12:42:58 +0530

    pathwa_bugs_partnership profile

--- HumanWisdom/projects/adults/src/app/adults/obstacles-enquiry/s43000/s43000.page.ts
+++ HumanWisdom/projects/adults/src/app/adults/obstacles-enquiry/s43000/s43000.page.ts
@@ -22,6 +22,8 @@ export class S43000Page implements OnInit,OnDestroy {
   startTime:any
   endTime:any
   totalTime:any
+    isContentsOpen = false;
+
   bookmark:any
   bookmarkList=[]
   pgResume: any;
@@ -116,6 +118,10 @@ export class S43000Page implements OnInit,OnDestroy {
 
     
   }
+
+  toggleContents() {
+      this.isContentsOpen = !this.isContentsOpen;
+    }
   toggleBookmark(){
     if(this.bookmark==0)
       this.bookmark=1
