079bf433f1bef2fa0aa71ed1715109261875bb35
bharti <himanigupta027@gmail.com>
2026-04-09 11:31:27 +0530

    communication

--- HumanWisdom/projects/teenagers/src/app/teenagers/communication/s132001/s132001.page.ts
+++ HumanWisdom/projects/teenagers/src/app/teenagers/communication/s132001/s132001.page.ts
@@ -133,6 +133,13 @@ export class S132001Page implements OnInit,OnDestroy {
       this.userId=JSON.parse(localStorage.getItem("userId"))
     }
 
+
+     // continue where you left    
+    this.service.clickModule(132, this.userId).subscribe(res => {
+      this.pgResume = (res.lastVisitedScreen != "") ? "s" + res.lastVisitedScreen : "";
+      this.lastvisited = res.lastVisitedScreen != "" ? true : false;
+    })
+    // /continue where you left
     if(!this.t) //if no token in url- not shared
     {
       if(this.loginResponse.Subscriber!=1 && !this.freeScreens.includes(this.screenNumber))
