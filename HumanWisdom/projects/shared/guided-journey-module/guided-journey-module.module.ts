241a2599788730e5973e0d280716d4406a92236d
Aditya Jha <adiitya1312@gmail.com>
2026-05-04 16:54:58 +0530

    module and component created for guided journey

---
+++ HumanWisdom/projects/shared/guided-journey-module/guided-journey-module.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GuidedJourneyModuleRoutingModule } from './guided-journey-module-routing.module';
+import { IonicModule } from '@ionic/angular';
+import { SharedModule } from '../shared.module';
+import { GuidedJourneyListingPage } from './guided-journey-listing/guided-journey-listing.page';
+import { GuidedJourneyIntroPage } from './guided-journey-intro/guided-journey-intro.page';
+import { GuidedJourneyDaysPage } from './guided-journey-days/guided-journey-days.page';
+import { GuidedJourneyEndPage } from './guided-journey-end/guided-journey-end.page';
+
+
+@NgModule({
+  declarations: [
+    GuidedJourneyListingPage,
+    GuidedJourneyIntroPage,
+    GuidedJourneyDaysPage,
+    GuidedJourneyEndPage
+  ],
+  imports: [
+    CommonModule,
+    IonicModule,
+    SharedModule,
+    GuidedJourneyModuleRoutingModule
+  ],
+  exports: [
+    GuidedJourneyListingPage,
+    GuidedJourneyIntroPage,
+    GuidedJourneyDaysPage,
+    GuidedJourneyEndPage
+  ]
+})
+export class GuidedJourneyModuleModule { }
