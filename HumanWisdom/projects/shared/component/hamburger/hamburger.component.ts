57b44ae03cdfdc62dbc0aa15a15ff549c7927c54
Aditya Jha <adiitya1312@gmail.com>
2026-05-04 11:59:24 +0530

    doble slash issue fixed

--- HumanWisdom/projects/shared/component/hamburger/hamburger.component.ts
+++ HumanWisdom/projects/shared/component/hamburger/hamburger.component.ts
@@ -140,10 +140,10 @@ export class HamburgerComponent implements OnInit, AfterViewInit, OnChanges, OnD
   onProgramChange() {
     if (this.isAdults) {
       this.logeventservice.logEvent('click_happiermeforteenagers');
-      window.location.href = environment.clientUrl + "/teenagers/home";
+      window.location.href = environment.clientUrl + "teenagers/home";
     } else {
       this.logeventservice.logEvent('click_happiermeforadults');
-      window.location.href = environment.clientUrl + '/adults/home';
+      window.location.href = environment.clientUrl + 'adults/home';
     }
   }
 
