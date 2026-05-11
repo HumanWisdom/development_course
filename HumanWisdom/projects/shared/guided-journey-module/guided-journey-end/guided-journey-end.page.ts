e03637909951bdc65613a81e24b110ac16ce2d32
Aditya Jha <adiitya1312@gmail.com>
2026-05-11 15:29:15 +0530

    guided journey back issue fixed, and continue button added

--- HumanWisdom/projects/shared/guided-journey-module/guided-journey-end/guided-journey-end.page.ts
+++ HumanWisdom/projects/shared/guided-journey-module/guided-journey-end/guided-journey-end.page.ts
@@ -118,12 +118,8 @@ export class GuidedJourneyEndPage implements OnInit {
   }
 
   goBack() {
-    var url = this.navigationService.navigateToBackLink();
-    if (url != null) {
-      this.router.navigateByUrl(url);
-    } else {
-      this.location.back();
-    }
+    const prefix = SharedService.getprogramName();
+    this.router.navigate([`/${prefix}/guided-journeys`]);
   }
 
   navigateToDay(day: number) {
