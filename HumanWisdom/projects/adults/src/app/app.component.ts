12790ec4952466a6fb241820ec6b8e452a53756f
Aditya Jha <adiitya1312@gmail.com>
2026-06-19 20:55:01 +0530

    auth token bug fixed

--- HumanWisdom/projects/adults/src/app/app.component.ts
+++ HumanWisdom/projects/adults/src/app/app.component.ts
@@ -111,7 +111,7 @@ export class AppComponent implements OnDestroy {
     
     SharedService.isIos = SharedService.initializeIosCheck(this.platform);
   
-    let urls = this.router.url.split('authtoken=');
+    let urls = window.location.href.split('authtoken=');
     if (urls && urls[1] == undefined) {
       if (localStorage.getItem("isloggedin") === 'T') {
         this.getUserInformationById(SharedService.getUserId());
--- HumanWisdom/projects/shared/component/olly-landing/olly-landing.component.ts
+++ HumanWisdom/projects/shared/component/olly-landing/olly-landing.component.ts
@@ -106,6 +106,7 @@ export class OllyLandingComponent implements OnInit, OnDestroy, OnChanges {
       if (authtoken) {
         this.service.setDataRecievedState(false);
         localStorage.setItem('socialLogin', 'T');
+        localStorage.setItem("token", JSON.stringify(authtoken));
         this.services.verifytoken(authtoken).subscribe((res) => {
           if (res) {
             localStorage.setItem("email", res['Email']);
@@ -116,8 +117,21 @@ export class OllyLandingComponent implements OnInit, OnDestroy, OnChanges {
             localStorage.setItem("Subscriber", res['Subscriber']);
             this.userId = res['UserId'];
             localStorage.setItem("userId", JSON.stringify(this.userId));
+            localStorage.setItem('isloggedin', 'T');
+            localStorage.setItem('guest', 'F');
+            localStorage.setItem("remember", 'T');
+            localStorage.setItem('adult', 'T');
             this.loginadult(res);
             this.service.setDataRecievedState(true);
+            
+            // Set the username dynamically so it displays immediately without reload
+            if (res['Name']) {
+              let namedata = res['Name'].split(' ');
+              this.username = namedata[0];
+              if (this.username) {
+                this.username = this.username.charAt(0).toUpperCase() + this.username.slice(1).toLowerCase();
+              }
+            }
           } else {
             localStorage.setItem("email", 'guest@humanwisdom.me');
             localStorage.setItem("pswd", '12345');
