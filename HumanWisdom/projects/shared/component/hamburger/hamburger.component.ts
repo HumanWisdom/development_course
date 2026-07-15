b790ca4d81d81486b6163d95a24a87f16e0c1780
bharti <himanigupta027@gmail.com>
2026-07-15 16:51:04 +0530

    bugs

--- HumanWisdom/projects/shared/component/hamburger/hamburger.component.ts
+++ HumanWisdom/projects/shared/component/hamburger/hamburger.component.ts
@@ -694,12 +694,12 @@ export class HamburgerComponent implements OnInit, AfterViewInit, OnChanges, OnD
         let detail = JSON.parse(userdetail);
         this.setProfileImage(detail);
       }
-      else {
-        console.log("url:" + (this.url))
-        if (this.url.toString().includes("https://") == false)
-          this.url = this.url === '' || this.url.includes('undefined') ?(this.isAdults? 'https://d1tenzemoxuh75.cloudfront.net/assets/svgs/v_1_4/profile_default.svg': 'https://d1tenzemoxuh75.cloudfront.net/assets/svgs/icons/user/profile_default.svg')  : '';
-        console.log("url:" + (this.url))
-      }
+      // else {
+      //   console.log("url:" + (this.url))
+      //   if (this.url.toString().includes("https://") == false)
+      //     this.url = this.url === '' || this.url.includes('undefined') ?(this.isAdults? 'https://d1tenzemoxuh75.cloudfront.net/assets/svgs/v_1_4/profile_default.svg': 'https://d1tenzemoxuh75.cloudfront.net/assets/svgs/icons/user/profile_default.svg')  : '';
+      //   console.log("url:" + (this.url))
+      // }
     }, 1000);
 
     // Add event listeners for submenu checkboxes to hide/show parent menu items
