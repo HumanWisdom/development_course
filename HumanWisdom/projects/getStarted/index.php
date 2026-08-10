a251da362e90f8f5842961d4f25ad0f1a8df8e70
bharti <himanigupta027@gmail.com>
2026-08-10 15:14:30 +0530

    index-page images updated and teenagers

--- HumanWisdom/projects/getStarted/index.php
+++ HumanWisdom/projects/getStarted/index.php
@@ -122,10 +122,10 @@ personal growth
           <div class="div">
             <div class="div-2">
               <img class="new-app-adults-teen display_m_none"  fetchpriority="high"
-                src="https://d1tenzemoxuh75.cloudfront.net/website/webp/bannerind.webp"
+                src="https://d1tenzemoxuh75.cloudfront.net/website/svgs/banneraug.svg"
                 alt="HappierMe app" />
              <img class="new-app-adults-teen display_d_none"  fetchpriority="high"
-                src="https://d1tenzemoxuh75.cloudfront.net/website/webp/bannermobile11.webp"
+                src="https://d1tenzemoxuh75.cloudfront.net/website/svgs/banner_mobile.svg"
                 alt="HappierMe app" />
           <div class="div-3">
                  <!-- rating row -->
